7074ab6a4a7d380d9ca3a17b8924982f98d262a2
Kshitij Gupta <kshitijgm@gmail.com>
2019-10-06 16:19:19 +0530

    fries: Code cleanup
    Signed-off-by: Kshitij Gupta <kshitijgm@gmail.com>

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:potato_fries/internal/common.dart';
-import 'package:potato_fries/internal/methods.dart';
 import 'package:potato_fries/ui/bottom_sheet.dart';
-import 'package:potato_fries/ui/croquette-badge.dart';
 
 BorderRadius _kBorderRadius = BorderRadius.circular(12);
 
@@ -11,19 +9,18 @@ void main() => runApp(PotatoFriesRoot());
 
 class PotatoFriesRoot extends StatelessWidget {
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Fries',
-      theme: ThemeData.light().copyWith(accentColor: Colors.blue),
-      darkTheme: ThemeData.dark().copyWith(accentColor: Colors.blue),
-      home: MyHomePage(),
-      debugShowCheckedModeBanner: false,
-    );
-  }
+  Widget build(context) => MaterialApp(
+        title: 'Fries',
+        theme: ThemeData.light().copyWith(accentColor: Colors.blue),
+        darkTheme: ThemeData.dark().copyWith(accentColor: Colors.blue),
+        home: MyHomePage(),
+        debugShowCheckedModeBanner: false,
+      );
 }
 
 class MyHomePage extends StatefulWidget {
-  @override createState() => _MyHomePageState();
+  @override
+  createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
@@ -36,92 +33,53 @@ class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
   @override
   void initState() {
     super.initState();
-    controller = AnimationController(vsync: this, duration: Duration(milliseconds: 300));
+    controller =
+        AnimationController(vsync: this, duration: Duration(milliseconds: 300));
   }
 
   @override
   Widget build(BuildContext context) {
-    bool isDarkTheme = MediaQuery.of(context).platformBrightness == Brightness.dark;
-
-    SystemChrome.setSystemUIOverlayStyle(
-      SystemUiOverlayStyle(
-        statusBarIconBrightness: isDarkTheme ? Brightness.light : Brightness.dark,
-        statusBarColor: Colors.transparent,
-        systemNavigationBarColor: Theme.of(context).scaffoldBackgroundColor,
-        systemNavigationBarIconBrightness: isDarkTheme ? Brightness.light : Brightness.dark
-      )
-    );
-    
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+      systemNavigationBarColor: Theme.of(context).scaffoldBackgroundColor,
+    ));
     return Scaffold(
       backgroundColor: Theme.of(context).cardColor,
+      appBar: null,
       body: Container(
         height: MediaQuery.of(context).size.height,
         child: Stack(
           children: <Widget>[
-            Card(
-              elevation: 0,
-              margin: EdgeInsets.all(0),
-              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(0)),
-              child: Container(
-                height: MediaQuery.of(context).padding.top + titleBarHeight,
-                alignment: Alignment.bottomCenter,
-                child: Container(
-                  padding: EdgeInsets.symmetric(horizontal: 24),
-                  width: MediaQuery.of(context).size.width,
-                  alignment: Alignment.centerLeft,
-                  height: titleBarHeight,
-                  child: Row(
-                    children: <Widget>[
-                      Text(
-                        FriesPage.pages[currentPage].title,
-                        style: TextStyle(
-                          fontWeight: FontWeight.w600,
-                          fontSize: 28,
-                        )
-                      ),
-                    ],
+            Positioned(
+              top: 0,
+              left: 0,
+              right: 0,
+              child: AppBar(
+                elevation: 0,
+                backgroundColor: Colors.transparent,
+                title: Text(
+                  FriesPage.pages[currentPage].title,
+                  style: TextStyle(
+                    fontWeight: FontWeight.w600,
                   ),
                 ),
               ),
             ),
             Container(
-              margin: EdgeInsets.only(top: MediaQuery.of(context).padding.top + titleBarHeight),
-              height: MediaQuery.of(context).size.height - (MediaQuery.of(context).padding.top + titleBarHeight) - 64,
+              margin: EdgeInsets.only(
+                  top: MediaQuery.of(context).padding.top + titleBarHeight),
+              height: MediaQuery.of(context).size.height -
+                  (MediaQuery.of(context).padding.top + titleBarHeight) -
+                  64,
               child: PageView.builder(
                 controller: pageController,
                 physics: NeverScrollableScrollPhysics(),
-                itemBuilder: (context, index) {
-                  return Center(
-                    child: Text(index.toString()),
-                  );
-                },
+                itemBuilder: (context, index) =>
+                    Center(child: Text(index.toString())),
                 itemCount: FriesPage.pages.length,
-                onPageChanged: (index) {
-                  setState(() => currentPage = index);
-                },
+                onPageChanged: (index) => setState(() => currentPage = index),
               ),
             ),
-            AnimatedBuilder(
-              animation: Tween<double>(begin: 0, end: 1).animate(controller),
-              builder: (context, child) {
-                return Visibility(
-                  visible: controller.value != 0,
-                  child: Opacity(
-                    opacity: controller.value,
-                    child: GestureDetector(
-                      onTapDown: (TapDownDetails details) {
-                        controller.reverse();
-                      },
-                      child: Container(
-                        height: MediaQuery.of(context).size.height,
-                        width: MediaQuery.of(context).size.width,
-                        color: Color(0x66000000),
-                      ),
-                    ),
-                  ),
-                );
-              },
-            ),
+            _sheetBackdrop,
             BottomAppSheet(
               controller: controller,
               currentPage: currentPage,
@@ -133,87 +91,24 @@ class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
           ],
         ),
       ),
-      /*body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          mainAxisSize: MainAxisSize.min,
-          children: <Widget>[
-            CroquetteBadge(),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Text('v3.0.0-beta+1'),
-            )
-          ],
-        ),
-      ),*/
-      //bottomNavigationBar: _bottomAppBar,
-      //floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-      //floatingActionButton: _floatingActionButton,
     );
   }
-}
 
-Widget get _floatingActionButton {
-  return Builder(
-    builder: (context) => FloatingActionButton(
-      onPressed: () {},
-      elevation: 0,
-      backgroundColor: HSLColor.fromColor(Theme.of(context).accentColor)
-          .withLightness(0.85)
-          .toColor(),
-      child: Icon(
-        Icons.home,
-        color: HSLColor.fromColor(Theme.of(context).accentColor)
-            .withLightness(0.4)
-            .toColor(),
-      ),
-    ),
-  );
-}
-
-Widget get _bottomAppBar {
-  return ClipRRect(
-    borderRadius: BorderRadius.only(
-      topLeft: _kBorderRadius.topLeft,
-      topRight: _kBorderRadius.topRight,
-    ),
-    child: Builder(
-      builder: (context) {
-        return BottomAppBar(
-          color: Theme.of(context).scaffoldBackgroundColor,
-          elevation: 0,
-          shape: CircularNotchedRectangle(),
-          child: Padding(
-            padding: const EdgeInsets.symmetric(vertical: 4.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: <Widget>[
-                Spacer(),
-                IconButton(
-                  icon: Icon(Icons.system_update),
-                  onPressed: () {},
-                ),
-                Spacer(),
-                IconButton(
-                  icon: Icon(Icons.person),
-                  onPressed: () => launchUrl("https://potatoproject.co/team"),
-                ),
-                Spacer(),
-                IconButton(
-                  icon: Icon(Icons.info),
-                  onPressed: () {},
-                ),
-                Spacer(),
-                IconButton(
-                  icon: Icon(Icons.keyboard_arrow_up),
-                  onPressed: () {},
-                ),
-                Spacer(),
-              ],
+  Widget get _sheetBackdrop => AnimatedBuilder(
+        animation: Tween<double>(begin: 0, end: 1).animate(controller),
+        builder: (context, child) => Visibility(
+          visible: controller.value != 0,
+          child: Opacity(
+            opacity: controller.value,
+            child: GestureDetector(
+              onTapDown: (TapDownDetails details) => controller.reverse(),
+              child: Container(
+                height: MediaQuery.of(context).size.height,
+                width: MediaQuery.of(context).size.width,
+                color: Color(0x66000000),
+              ),
             ),
           ),
-        );
-      },
-    ),
-  );
+        ),
+      );
 }
--- lib/ui/bottom_sheet.dart
+++ lib/ui/bottom_sheet.dart
@@ -3,10 +3,10 @@ import 'package:potato_fries/internal/common.dart';
 import 'package:potato_fries/internal/methods.dart';
 import 'package:potato_fries/ui/sizeable_list_tile.dart';
 
-class BottomAppSheet extends StatefulWidget {
-  AnimationController controller;
-  int currentPage;
-  Function(int index) onItemClick;
+class BottomAppSheet extends StatelessWidget {
+  final AnimationController controller;
+  final int currentPage;
+  final Function(int index) onItemClick;
 
   BottomAppSheet({
     @required this.controller,
@@ -14,26 +14,25 @@ class BottomAppSheet extends StatefulWidget {
     @required this.onItemClick,
   });
 
-  @override
-  createState() => _BottomAppSheetState();
-}
+  Animation<double> _iconTurns;
+  Animation<double> _positionChange;
 
-class _BottomAppSheetState extends State<BottomAppSheet> {
+  static double totalHeight = 2.0 + 64 + (50 * FriesPage.pages.length);
+  static Animatable<double> _halfTween = Tween<double>(begin: 0.0, end: 0.5);
+  static Animatable<double> _positionTween =
+      Tween<double>(begin: 64, end: totalHeight);
+  static Animatable<double> _easeInTween = CurveTween(curve: Curves.easeIn);
+
   @override
   Widget build(BuildContext context) {
-    double totalHeight = 2.0 + 64 + (50 * FriesPage.pages.length);
-    Animatable<double> _halfTween = Tween<double>(begin: 0.0, end: 0.5);
-    Animatable<double> _positionTween = Tween<double>(begin: 64, end: totalHeight);
-    Animatable<double> _easeInTween = CurveTween(curve: Curves.easeIn);
-
-    Animation<double> _iconTurns = widget.controller.drive(_halfTween.chain(_easeInTween));
-    Animation<double> _positionChange = widget.controller.drive(_positionTween.chain(_easeInTween));
-
+    _iconTurns ??= controller.drive(_halfTween.chain(_easeInTween));
+    _positionChange ??= controller.drive(_positionTween.chain(_easeInTween));
     return AnimatedBuilder(
       animation: _positionChange,
       builder: (context, child) => Positioned(
         width: MediaQuery.of(context).size.width,
-        top: (MediaQuery.of(context).size.height - 64) - (_positionChange.value - 64),
+        top: (MediaQuery.of(context).size.height - 64) -
+            (_positionChange.value - 64),
         child: GestureDetector(
           onVerticalDragUpdate: _onDrag,
           onVerticalDragEnd: _onDragEnd,
@@ -41,91 +40,88 @@ class _BottomAppSheetState extends State<BottomAppSheet> {
             color: Theme.of(context).scaffoldBackgroundColor,
             elevation: 6,
             shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.only(
-                topLeft: Radius.circular(12),
-                topRight: Radius.circular(12),
-              )
-            ),
+                borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(12),
+              topRight: Radius.circular(12),
+            )),
             margin: EdgeInsets.all(0),
             child: Column(
               children: <Widget>[
                 Container(
-                  height: 64,
-                  child: Stack(
-                    alignment: Alignment.center,
-                    overflow: Overflow.visible,
-                    children: <Widget>[
-                      Container(
-                        height: 64,
-                        padding: EdgeInsets.symmetric(horizontal: 16),
-                        child: Row(
-                          children: <Widget>[
-                            Padding(
-                              padding: EdgeInsets.all(8),
-                              child: Icon(FriesPage.pages[widget.currentPage].icon),
-                            ),
-                            Spacer(flex: 3),
-                            IconButton(
-                              icon: Icon(Icons.system_update),
-                              onPressed: () {},
-                              tooltip: "Open Potato Center",
-                            ),
-                            Spacer(),
-                            IconButton(
-                              icon: Icon(Icons.person),
-                              onPressed: () => launchUrl("https://potatoproject.co/team"),
-                              tooltip: "Discover POSP team",
-                            ),
-                            Spacer(),
-                            IconButton(
-                              icon: Icon(Icons.info),
-                              onPressed: () {},
-                              tooltip: "General info",
-                            ),
-                            Spacer(flex: 3),
-                            IconButton(
-                              icon: RotationTransition(
-                                turns: _iconTurns,
-                                child: Icon(
-                                  Icons.expand_less,
-                                  color: Theme.of(context).accentColor,
+                    height: 64,
+                    child: Stack(
+                      alignment: Alignment.center,
+                      overflow: Overflow.visible,
+                      children: <Widget>[
+                        Container(
+                          height: 64,
+                          padding: EdgeInsets.symmetric(horizontal: 16),
+                          child: Row(
+                            children: <Widget>[
+                              Padding(
+                                padding: EdgeInsets.all(8),
+                                child: Icon(FriesPage.pages[currentPage].icon),
+                              ),
+                              Spacer(flex: 3),
+                              IconButton(
+                                icon: Icon(Icons.system_update),
+                                onPressed: () {},
+                                tooltip: "Open Potato Center",
+                              ),
+                              Spacer(),
+                              IconButton(
+                                icon: Icon(Icons.person),
+                                onPressed: () =>
+                                    launchUrl("https://potatoproject.co/team"),
+                                tooltip: "Discover POSP team",
+                              ),
+                              Spacer(),
+                              IconButton(
+                                icon: Icon(Icons.info),
+                                onPressed: () {},
+                                tooltip: "Build info",
+                              ),
+                              Spacer(flex: 3),
+                              IconButton(
+                                icon: RotationTransition(
+                                  turns: _iconTurns,
+                                  child: Icon(
+                                    Icons.expand_less,
+                                    color: Theme.of(context).accentColor,
+                                  ),
                                 ),
+                                onPressed: () {
+                                  if (controller.status ==
+                                      AnimationStatus.completed) {
+                                    controller.reverse();
+                                  } else {
+                                    controller.forward();
+                                  }
+                                },
                               ),
-                              onPressed: () {
-                                if(widget.controller.status == AnimationStatus.completed) {
-                                  widget.controller.reverse();
-                                } else {
-                                  widget.controller.forward();
-                                }
-                              },
-                            ),
-                          ],
-                        ),
-                      ),
-                    ],
-                  )
-                ),
-                Visibility(
-                  visible: widget.controller.value > 0,
-                  child: AnimatedBuilder(
-                    animation: Tween<double>(begin: 0, end: 1).animate(widget.controller),
-                    builder: (context, child) {
-                      return Opacity(
-                        opacity: widget.controller.value,
-                        child: SingleChildScrollView(
-                          child: Column(
-                            children: [
-                              Divider(
-                                height: 2,
-                              ),
-                              Column(
-                                children: pageSwitcherItemsBuilder,
-                              ),
-                            ]
+                            ],
                           ),
                         ),
-                      );
-                    },
+                      ],
+                    )),
+                Visibility(
+                  visible: controller.value > 0,
+                  child: AnimatedBuilder(
+                    animation:
+                        Tween<double>(begin: 0, end: 1).animate(controller),
+                    builder: (context, child) => Opacity(
+                      opacity: controller.value,
+                      child: SingleChildScrollView(
+                        child: Column(children: [
+                          Divider(
+                            height: 2,
+                          ),
+                          Column(
+                            children: pageSwitcherItemsBuilder,
+                          ),
+                        ]),
+                      ),
+                    ),
                   ),
                 ),
               ],
@@ -139,18 +135,18 @@ class _BottomAppSheetState extends State<BottomAppSheet> {
   List<Widget> get pageSwitcherItemsBuilder {
     List<Widget> widgets = [];
 
-    for(int i = 0; i < FriesPage.pages.length; i++) {
+    for (int i = 0; i < FriesPage.pages.length; i++) {
       widgets.add(
-        SizeableListTile(
-          icon: Icon(FriesPage.pages[i].icon),
-          title: FriesPage.pages[i].title,
-          height: 50,
-          onTap: () {
-            widget.onItemClick(i);
-          },
-          elementsColor: Theme.of(context).textTheme.title.color,
-          selected: widget.currentPage == i,
-          selectedColor: Theme.of(context).accentColor,
+        Builder(
+          builder: (context) => SizeableListTile(
+            icon: Icon(FriesPage.pages[i].icon),
+            title: FriesPage.pages[i].title,
+            height: 50,
+            onTap: () => onItemClick(i),
+            elementsColor: Theme.of(context).textTheme.title.color,
+            selected: currentPage == i,
+            selectedColor: Theme.of(context).accentColor,
+          ),
         ),
       );
     }
@@ -158,37 +154,33 @@ class _BottomAppSheetState extends State<BottomAppSheet> {
     return widgets;
   }
 
-  void _onDrag(DragUpdateDetails details) {
-    widget.controller.value -= details.primaryDelta / ((50 * 7.0) + 4 + 64 - 64);
-  }
+  void _onDrag(DragUpdateDetails details) =>
+      controller.value -= details.primaryDelta / ((50 * 7.0) + 4 + 64 - 64);
 
   void _onDragEnd(DragEndDetails details) {
     double minFlingVelocity = 365.0;
 
     //let the current animation finish before starting a new one
-    if(widget.controller.isAnimating) return;
+    if (controller.isAnimating) return;
 
     //check if the velocity is sufficient to constitute fling
-    if(details.velocity.pixelsPerSecond.dy.abs() >= minFlingVelocity){
-      double visualVelocity = - details.velocity.pixelsPerSecond.dy / ((50 * 7.0) + 4 + 64 - 64);
+    if (details.velocity.pixelsPerSecond.dy.abs() >= minFlingVelocity) {
+      double visualVelocity =
+          -details.velocity.pixelsPerSecond.dy / ((50 * 7.0) + 4 + 64 - 64);
 
-      widget.controller.fling(velocity: visualVelocity);
+      controller.fling(velocity: visualVelocity);
 
       return;
     }
-    
-    if(widget.controller.value > 0.5)
+
+    if (controller.value > 0.5)
       _open();
     else
       _close();
   }
 
-  void _close(){
-    widget.controller.fling(velocity: -1.0);
-  }
+  void _close() => controller.fling(velocity: -1.0);
 
   //open the panel
-  void _open(){
-    widget.controller.fling(velocity: 1.0);
-  }
+  void _open() => controller.fling(velocity: 1.0);
 }
