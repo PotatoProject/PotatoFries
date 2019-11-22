6e53ced6fdd02b2d482014fa56f7ca17b7c7ccdf
neddstarkk <nedheesh.hasija@gmail.com>
2019-11-22 17:07:26 +0530

    Build Info: Added an AlertDialog on button press

--- lib/ui/bottom_sheet.dart
+++ lib/ui/bottom_sheet.dart
@@ -2,8 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:potato_fries/internal/methods.dart';
 import 'package:potato_fries/internal/page_data.dart';
 import 'package:potato_fries/ui/sizeable_list_tile.dart';
+import 'package:potato_fries/ui/croquette_badge.dart';
+import 'package:potato_fries/ui/svg_icon.dart';
 
-class BottomAppSheet extends StatelessWidget {
+class BottomAppSheet extends StatefulWidget {
   final AnimationController controller;
   final int currentPage;
   final Function(int index) onItemClick;
@@ -24,15 +26,50 @@ class BottomAppSheet extends StatelessWidget {
   static Animatable<double> _easeInTween = CurveTween(curve: Curves.easeIn);
 
   @override
+  _BottomAppSheetState createState() => _BottomAppSheetState();
+}
+
+class _BottomAppSheetState extends State<BottomAppSheet> {
+  void _showDialog() {
+    // flutter defined function
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        // return object of type Dialog
+        return AlertDialog(
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              CroquetteBadge(),
+              SizedBox(height: 10.0),
+              Text("BUILD INFO HERE"),
+            ],
+          ),
+          actions: <Widget>[
+            // usually buttons at the bottom of the dialog
+            new FlatButton(
+              child: new Text("CLOSE"),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  @override
   Widget build(BuildContext context) {
-    _iconTurns ??= controller.drive(_halfTween.chain(_easeInTween));
-    _positionChange ??= controller.drive(_positionTween.chain(_easeInTween));
+    BottomAppSheet._iconTurns ??= widget.controller.drive(BottomAppSheet._halfTween.chain(BottomAppSheet._easeInTween));
+    BottomAppSheet._positionChange ??= widget.controller.drive(BottomAppSheet._positionTween.chain(BottomAppSheet._easeInTween));
     return AnimatedBuilder(
-      animation: _positionChange,
+      animation: BottomAppSheet._positionChange,
       builder: (context, child) => Positioned(
         width: MediaQuery.of(context).size.width,
         top: (MediaQuery.of(context).size.height - 64) -
-            (_positionChange.value - 64),
+            (BottomAppSheet._positionChange.value - 64),
         child: GestureDetector(
           onVerticalDragUpdate: _onDrag,
           onVerticalDragEnd: _onDragEnd,
@@ -61,7 +98,7 @@ class BottomAppSheet extends StatelessWidget {
                           children: <Widget>[
                             Padding(
                               padding: EdgeInsets.all(8),
-                              child: Icon((pages[currentPage] as dynamic).icon),
+                              child: Icon((pages[widget.currentPage] as dynamic).icon),
                             ),
                             Spacer(flex: 3),
                             IconButton(
@@ -79,22 +116,22 @@ class BottomAppSheet extends StatelessWidget {
                             Spacer(),
                             IconButton(
                               icon: Icon(Icons.info),
-                              onPressed: () {},
+                              onPressed: _showDialog,
                               tooltip: "Build info",
                             ),
                             Spacer(flex: 3),
                             IconButton(
                               icon: RotationTransition(
-                                turns: _iconTurns,
+                                turns: BottomAppSheet._iconTurns,
                                 child: Icon(
                                   Icons.expand_less,
                                   color: Theme.of(context).accentColor,
                                 ),
                               ),
                               onPressed: () =>
-                                  controller.status == AnimationStatus.completed
-                                      ? controller.reverse()
-                                      : controller.forward(),
+                                  widget.controller.status == AnimationStatus.completed
+                                      ? widget.controller.reverse()
+                                      : widget.controller.forward(),
                             ),
                           ],
                         ),
@@ -103,12 +140,12 @@ class BottomAppSheet extends StatelessWidget {
                   ),
                 ),
                 Visibility(
-                  visible: controller.value > 0,
+                  visible: widget.controller.value > 0,
                   child: AnimatedBuilder(
                     animation:
-                        Tween<double>(begin: 0, end: 1).animate(controller),
+                        Tween<double>(begin: 0, end: 1).animate(widget.controller),
                     builder: (context, child) => Opacity(
-                      opacity: controller.value,
+                      opacity: widget.controller.value,
                       child: SingleChildScrollView(
                         child: Column(
                           children: <Widget>[
@@ -138,9 +175,9 @@ class BottomAppSheet extends StatelessWidget {
             icon: Icon((pages[i] as dynamic).icon),
             title: (pages[i] as dynamic).title,
             height: 50,
-            onTap: () => onItemClick(i),
+            onTap: () => widget.onItemClick(i),
             elementsColor: Theme.of(context).textTheme.title.color,
-            selected: currentPage == i,
+            selected: widget.currentPage == i,
             selectedColor: Theme.of(context).accentColor,
           ),
         ),
@@ -151,32 +188,31 @@ class BottomAppSheet extends StatelessWidget {
   }
 
   void _onDrag(DragUpdateDetails details) =>
-      controller.value -= details.primaryDelta / ((50 * 7.0) + 4 + 64 - 64);
+      widget.controller.value -= details.primaryDelta / ((50 * 7.0) + 4 + 64 - 64);
 
   void _onDragEnd(DragEndDetails details) {
     double minFlingVelocity = 365.0;
 
     //let the current animation finish before starting a new one
-    if (controller.isAnimating) return;
+    if (widget.controller.isAnimating) return;
 
     //check if the velocity is sufficient to constitute fling
     if (details.velocity.pixelsPerSecond.dy.abs() >= minFlingVelocity) {
       double visualVelocity =
           -details.velocity.pixelsPerSecond.dy / ((50 * 7.0) + 4 + 64 - 64);
 
-      controller.fling(velocity: visualVelocity);
+      widget.controller.fling(velocity: visualVelocity);
 
       return;
     }
 
-    if (controller.value > 0.5)
+    if (widget.controller.value > 0.5)
       _open();
     else
       _close();
   }
 
-  void _close() => controller.fling(velocity: -1.0);
+  void _close() => widget.controller.fling(velocity: -1.0);
 
-  //open the panel
-  void _open() => controller.fling(velocity: 1.0);
+  void _open() => widget.controller.fling(velocity: 1.0);
 }
