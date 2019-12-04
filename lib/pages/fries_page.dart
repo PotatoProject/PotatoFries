961229426b06c7f557dd289e0c19dc0b56a39efb
HrX03 <dn.bianco03@gmail.com>
2019-12-04 21:00:39 +0100

    fries: New header design

--- lib/pages/fries_page.dart
+++ lib/pages/fries_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:potato_fries/internal/methods.dart';
+import 'package:potato_fries/ui/croquette_badge.dart';
 
 class FriesPage extends StatelessWidget {
   final String title;
@@ -13,18 +15,98 @@ class FriesPage extends StatelessWidget {
         assert(title != null);
 
   @override
-  Widget build(BuildContext context) => Column(
+  Widget build(BuildContext context) {
+    void _showDialog() {
+      // flutter defined function
+      showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          // return object of type Dialog
+          return AlertDialog(
+            content: Column(
+              mainAxisSize: MainAxisSize.min,
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                CroquetteBadge(),
+                SizedBox(height: 10.0),
+                Text("BUILD INFO HERE"),
+              ],
+            ),
+            actions: <Widget>[
+              // usually buttons at the bottom of the dialog
+              new FlatButton(
+                child: new Text("CLOSE"),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+              ),
+            ],
+          );
+        },
+      );
+    }
+
+    return Column(
       children: <Widget>[
         AppBar(
           elevation: 0,
-          backgroundColor: Colors.transparent,
+          backgroundColor: Theme.of(context).scaffoldBackgroundColor,
           title: Text(
             title,
             style: TextStyle(
-              fontWeight: FontWeight.w600,
-              color: Theme.of(context).textTheme.body1.color,
+              //fontWeight: FontWeight.w600,
+              //color: Theme.of(context).textTheme.body1.color,
             ),
           ),
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(Icons.search),
+              onPressed: () {},
+            ),
+            PopupMenuButton(
+              elevation: 3,
+              itemBuilder: (context) {
+                return [
+                  PopupMenuItem(
+                    value: 0,
+                    child: ListTile(
+                      leading: Icon(Icons.system_update),
+                      title: Text("PotatoCenter"),
+                      contentPadding: EdgeInsets.all(0),
+                    ),
+                  ),
+                  PopupMenuItem(
+                    value: 1,
+                    child: ListTile(
+                      leading: Icon(Icons.person),
+                      title: Text("Discover POSP team"),
+                      contentPadding: EdgeInsets.all(0),
+                    ),
+                  ),
+                  PopupMenuItem(
+                    value: 2,
+                    child: ListTile(
+                      leading: Icon(Icons.info),
+                      title: Text("Build info"),
+                      contentPadding: EdgeInsets.all(0),
+                    ),
+                  ),
+                ];
+              },
+              onSelected: (value) {
+                switch(value) {
+                  case 0:
+                    break;
+                  case 1:
+                    launchUrl("https://potatoproject.co/team");
+                    break;
+                  case 2:
+                    _showDialog();
+                    break;
+                }
+              },
+            ),
+          ],
         ),
         Container(child: header),
         Expanded(
@@ -38,4 +120,5 @@ class FriesPage extends StatelessWidget {
         ),
       ],
     );
+  }
 }
