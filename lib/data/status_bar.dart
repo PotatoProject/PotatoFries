915d8a68970b1a6bd43a7a2de9e74921b8e03949
rajadeja <raj.jdja@gmail.com>
2020-01-09 21:19:32 +0530

    Statusbar: Add toggle for POSP logo
    Change-Id: I49c43cc0efd4560b69f4e26c4de9072fdbb9be88

--- lib/data/status_bar.dart
+++ lib/data/status_bar.dart
@@ -1,1 +1,20 @@
-final Map<String, dynamic> statusBar = {};
+import 'package:android_flutter_settings/android_flutter_settings.dart';
+import 'package:flutter/material.dart';
+import 'package:potato_fries/widgets/directory.dart';
+
+final Map<String, dynamic> statusBar = {
+  'StatusBar Items': statusbaricons,
+};
+
+final Map<String, dynamic> statusbaricons = {
+  'status_bar_logo': {
+    'title': 'POSP swag',
+    'subtitle': 'Show off POSP logo on statusbar',
+    'icon': Icons.crop_portrait,
+    'widget': WidgetType.SWITCH,
+    'setting_type': SettingType.SYSTEM,
+    'widget_data': {
+      'default': false,
+    }
+  },
+};
