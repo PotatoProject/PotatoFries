c0cd8d68282a7f763d072fa442e51dea80f33eff
Ashwin Prakash R C <ashwin2001achu@gmail.com>
2020-01-09 13:55:49 +0530

    Fries: add toggle for POSP logo in the status bar
    reffered from: LorDClockaN
    
    Change-Id: I7b4e4e82879383fe9df7adfa70508eb7b2cd0852

--- lib/data/status_bar.dart
+++ lib/data/status_bar.dart
@@ -1,1 +1,19 @@
-final Map<String, dynamic> statusBar = {};
+import 'package:android_flutter_settings/android_flutter_settings.dart';
+import 'package:flutter/material.dart';
+import 'package:potato_fries/widgets/directory.dart';
+
+final Map<String, dynamic> statusBar = {
+  'Our icons': sbicon,
+};
+
+final Map<String, dynamic> sbicon = {
+  'status_bar_logo': {
+    'title': 'POSP icon in statusbar',
+    'subtitle': 'Let everyone know you are using POSP',
+    'widget': WidgetType.SWITCH,
+    'setting_type': SettingType.SYSTEM,
+    'widget_data': {
+      'default': false,
+    }
+  },
+};
