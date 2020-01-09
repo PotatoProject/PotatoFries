1e3cb1046076ce0c695c71dd7844766cac197c97
rajadeja <raj.jdja@gmail.com>
2020-01-09 21:18:40 +0530

    Lockscreen: Option to enable/disable LS media art
    Change-Id: Ic59b23f1530a7e357ce6f14ab742d85c525dc365

--- lib/data/lock_screen.dart
+++ lib/data/lock_screen.dart
@@ -1,1 +1,20 @@
-final Map<String, dynamic> lockScreen = {};
+import 'package:android_flutter_settings/android_flutter_settings.dart';
+import 'package:flutter/material.dart';
+import 'package:potato_fries/widgets/directory.dart';
+
+final Map<String, dynamic> lockScreen = {
+  'Lockscreen Stuff': lsstuff,
+};
+
+final Map<String, dynamic> lsstuff ={
+  'lockscreen_media_metadata': {
+    'title': 'Lockscreen media art',
+    'subtitle': 'Show album art on lockscreen',
+    'icon': Icons.image,
+    'widget': WidgetType.SWITCH,
+    'setting_type': SettingType.SYSTEM,
+    'widget_data': {
+      'default': true,
+    }
+  },
+};
