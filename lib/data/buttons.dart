43b90181d76c0ff2ac2c6d0dad1313a26ae701f3
Ashwin Prakash R C <ashwin2001achu@gmail.com>
2020-01-09 14:15:05 +0530

    Fries: add toggle for notification sound if screen on
    Reffered from: Ezio84
    
    Change-Id: Ifcc2fbecc696cc6f9efa5d4f9f3417e5b6136b2c

--- lib/data/buttons.dart
+++ lib/data/buttons.dart
@@ -1,1 +1,20 @@
-final Map<String, dynamic> buttons = {};
+import 'package:android_flutter_settings/android_flutter_settings.dart';
+import 'package:flutter/material.dart';
+import 'package:potato_fries/widgets/directory.dart';
+
+final Map<String, dynamic> buttons = {
+  'Notifications': notif,
+};
+
+final Map<String, dynamic> notif = {
+  'notification_sound_vib_screen_on':{
+    'title': 'Notification sound if active',
+    'subtitle': 'Play sound and vibration for notifications when screen is on',
+    'widget' : WidgetType.SWITCH,
+    'setting_type': SettingType.SYSTEM,
+        'widget_data': {
+      'default': true,
+    }
+
+  },
+};
