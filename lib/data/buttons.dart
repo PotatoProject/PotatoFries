b87c3ec43afc1f10fee153732580f13bdf8115de
rajadeja <raj.jdja@gmail.com>
2020-01-09 13:42:22 +0530

    Buttons & gestures: Add stuff
    -Add volume button to skip track
    -Add double tap on statusbar to sleep
    
    Change-Id: I474ca6683557f957b28ade46a45dfe0b28bce3c5
    Signed-off-by: Kshitij Gupta <kshitijgm@gmail.com>

--- lib/data/buttons.dart
+++ lib/data/buttons.dart
@@ -1,1 +1,36 @@
-final Map<String, dynamic> buttons = {};
+import 'package:android_flutter_settings/android_flutter_settings.dart';
+import 'package:flutter/material.dart';
+import 'package:potato_fries/widgets/directory.dart';
+
+final Map<String, dynamic> buttons = {
+  'System Buttons': sysbutton,
+  'System Gestures': systemgesture,
+};
+
+final Map<String, dynamic> sysbutton = {
+  'volume_button_music_control': {
+    'title': 'Volume button to skip tracks',
+    'subtitle': 'Long press volume buttons to forward/backward track',
+    'icon': Icons.music_note,
+    'widget': WidgetType.SWITCH,
+    'setting_type': SettingType.SYSTEM,
+    'widget_data': {
+      'default': false,
+    },
+    'version': '3.1.1',
+  },
+};
+
+final Map<String, dynamic> systemgesture = {
+  'double_tap_sleep_gesture': {
+    'title': 'Double tap Status bar to sleep',
+    'subtitle': 'Tap twice on Status bar to put device in sleep',
+    'icon': Icons.touch_app,
+    'widget': WidgetType.SWITCH,
+    'setting_type': SettingType.SYSTEM,
+    'widget_data': {
+      'default': false,
+    },
+    'version': '3.1.1',
+  },
+};
