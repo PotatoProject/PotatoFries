5025629810d4fc70a6dc6145ac8fa6b809401ad3
Ashwin Prakash R C <ashwin2001achu@gmail.com>
2020-01-09 13:13:30 +0530

    Fries: Add toggle to disable lockscreen media art
    -Reffered from beanstown106
    
    Change-Id: I763c6509b5116b497787e11236f8c448116ba413

--- lib/data/lock_screen.dart
+++ lib/data/lock_screen.dart
@@ -1,1 +1,19 @@
-final Map<String, dynamic> lockScreen = {};
+import 'package:android_flutter_settings/android_flutter_settings.dart';
+import 'package:flutter/material.dart';
+import 'package:potato_fries/widgets/directory.dart';
+
+final Map<String, dynamic> lockScreen = {
+  'Lockscreen Media': lsart,
+};
+
+final Map<String, dynamic> lsart = {
+  'lockscreen_media_metadata': {
+    'title': 'Lockscreen Media Art',
+    'subtitle': 'Show Album Art on lockscreen',
+    'widget': WidgetType.SWITCH,
+    'setting_type': SettingType.SYSTEM,
+    'widget_data': {
+      'default': true,
+    }
+  },
+};
