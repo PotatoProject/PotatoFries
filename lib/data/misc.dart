8a01f78ed317fdc110e69a8b1cb1e330dd24896e
Kshitij Gupta <kshitijgm@gmail.com>
2020-01-15 15:40:45 +0530

    fries: misc: Add signature spoofing toggle
    Change-Id: I9b2a5676e0906c39844953d7a6919ac51ee31cd3

--- lib/data/misc.dart
+++ lib/data/misc.dart
@@ -1,1 +1,21 @@
-final Map<String, dynamic> misc = {};
+import 'package:android_flutter_settings/android_flutter_settings.dart';
+import 'package:flutter/material.dart';
+import 'package:potato_fries/widgets/directory.dart';
+
+final Map<String, dynamic> misc = {
+  'Signature spoofing': spoofing,
+};
+
+final Map<String, dynamic> spoofing = {
+  'allow_signature_fake': {
+    'title': 'Toggle signature spoofing',
+    'subtitle': 'Allow fake signatures',
+    'icon': Icons.format_color_text,
+    'widget': WidgetType.SWITCH,
+    'setting_type': SettingType.GLOBAL,
+    'widget_data': {
+      'default': true,
+    }
+  },
+};
+
