192599f059eca2eeec36f30a4ae854ce36b17176
Kshitij Gupta <kshitijgm@gmail.com>
2020-01-27 00:17:55 +0530

    fries: Handle newer selinux friendly settings impl for accenter
    - The new impl is controlled via settings. UiModeManager observes the 2
      settings and does setProp as necessary.
    - Perform a version check to retain legacy impl functionality for older
      builds
    - Also upgrade deps
    
    Change-Id: I8e6316f85604ac18ddcd93d006f90080812fe228

--- lib/widgets/custom/accent_picker.dart
+++ lib/widgets/custom/accent_picker.dart
@@ -1,6 +1,7 @@
 import 'package:android_flutter_settings/android_flutter_settings.dart';
 import 'package:flutter/material.dart';
 import 'package:potato_fries/provider/app_info.dart';
+import 'package:potato_fries/utils/methods.dart';
 import 'package:potato_fries/widgets/color_picker_dual.dart';
 import 'package:provider/provider.dart';
 
@@ -17,17 +18,36 @@ class AccentPicker extends StatelessWidget {
         lightnessDeltaCenter: 0.15,
         lightnessDeltaEnd: 0.6,
         onApply: (String newDark, String newLight) {
-          AndroidFlutterSettings.setProp(
-            'persist.sys.theme.accent_dark',
-            newDark,
-          );
-          AndroidFlutterSettings.setProp(
-              'persist.sys.theme.accent_light', newLight);
+          var hostVer =
+              Provider.of<AppInfoProvider>(context, listen: false).hostVersion;
+          var settingImplVer = '3.1.1';
+          var settingImpl = isVersionCompatible(settingImplVer, hostVer);
+          if (settingImpl) {
+            AndroidFlutterSettings.setProp(
+              'persist.sys.theme.accent_dark',
+              newDark,
+            );
+            AndroidFlutterSettings.setProp(
+              'persist.sys.theme.accent_light',
+              newLight,
+            );
+          } else {
+            AndroidFlutterSettings.putString(
+              'sys_accent_light',
+              newLight,
+              SettingType.SECURE,
+            );
+            AndroidFlutterSettings.putString(
+              'sys_accent_dark',
+              newDark,
+              SettingType.SECURE,
+            );
+          }
           AndroidFlutterSettings.reloadAssets('com.android.settings');
           AndroidFlutterSettings.reloadAssets('com.android.systemui');
-          Provider.of<AppInfoProvider>(context).accentDark =
+          Provider.of<AppInfoProvider>(context, listen: false).accentDark =
               Color(int.parse('0xff' + newLight));
-          Provider.of<AppInfoProvider>(context).accentLight =
+          Provider.of<AppInfoProvider>(context, listen: false).accentLight =
               Color(int.parse('0xff' + newDark));
         },
       );
