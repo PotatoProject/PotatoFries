23b2d8929f2dfa37303c49484a875f566b16fd0a
rajadeja <rajsjadeja4@gmail.com>
2020-02-03 23:07:08 +0530

    Add clock customizations
    note to self: Only show/hide & am/pm style is done
    
    Change-Id: I97e37dd397294729293b1d3229847c61cd634190

--- lib/data/status_bar.dart
+++ lib/data/status_bar.dart
@@ -4,6 +4,7 @@ import 'package:potato_fries/widgets/directory.dart';
 
 final Map<String, dynamic> statusBar = {
   'StatusBar Icons': statusbaricons,
+  'Clock': statusbarclock,
 };
 
 final Map<String, dynamic> statusbaricons = {
@@ -35,6 +36,36 @@ final Map<String, dynamic> statusbaricons = {
     'setting_type': SettingType.SYSTEM,
     'widget_data': {
       'default': true,
+    },
+    'version': '3.1.1',
+  },
+};
+
+final Map<String, dynamic> statusbarclock = {
+'status_bar_clock': {
+'title': 'Hide or show clock on statusbar',
+'icon': Icons.crop_portrait,
+'widget': WidgetType.DROPDOWN,
+'setting_type': SettingType.SECURE,
+'widget_data': {
+'values': {
+  '0': 'Hide' ,
+  '1': 'Show' ,
+      }
+    },
+  'version': '3.1.1',
+  },
+  'statusbar_clock_am_pm_style': {
+    'title': 'AM/PM style',
+    'icon': Icons.crop_portrait,
+    'widget': WidgetType.DROPDOWN,
+    'setting_type': SettingType.SECURE,
+    'widget_data': {
+      'values': {
+        '0': 'Hide AM/PM' ,
+        '1': 'Small AM/PM' ,
+        '2': 'Normal AM/PM' ,
+      }
     },
     'version': '3.1.1',
   },
