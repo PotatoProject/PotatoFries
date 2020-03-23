bdd64361c9548b19bc8efffa80d591724ece7454
rajadeja <rajsjadeja4@gmail.com>
2020-03-23 14:57:51 +0530

    some custs
    Change-Id: I0f12f6715bb98e275c2a84e5901e8e2501171b0d

--- lib/data/status_bar.dart
+++ lib/data/status_bar.dart
@@ -5,6 +5,7 @@ import 'package:potato_fries/widgets/directory.dart';
 final Map<String, dynamic> statusBar = {
   'StatusBar Icons': statusbaricons,
   'Clock': statusbarclock,
+  'Network Traffic Monitor': netTraffic,
 };
 
 final Map<String, dynamic> statusbaricons = {
@@ -39,102 +40,142 @@ final Map<String, dynamic> statusbaricons = {
     },
     'version': '3.1.1',
   },
+'use_old_mobiletype': {
+'title': 'Old mobile data icons',
+'subtitle': 'Merge signal and type icons',
+'widget': WidgetType.SWITCH,
+'setting_type': SettingType.SYSTEM,
+'widget_data': {
+'default': false,
+    },
+  },
 };
 
 final Map<String, dynamic> statusbarclock = {
   'smart_clock_enable': {
-    'title': 'Smart Clock',
-    'subtitle': 'Show statusbar clock periodically',
-    'icon': Icons.fiber_smart_record,
-    'widget': WidgetType.SWITCH,
-    'setting_type': SettingType.SYSTEM,
+    'title': 'Smart Clock' ,
+    'subtitle': 'Show statusbar clock periodically' ,
+    'icon': Icons.fiber_smart_record ,
+    'widget': WidgetType.SWITCH ,
+    'setting_type': SettingType.SYSTEM ,
     'widget_data': {
-      'default': false,
-    },
-    'version': '3.1.3',
-  },
-'status_bar_clock': {
-'title': 'Hide or show clock on statusbar',
-'icon': Icons.crop_portrait,
-'widget': WidgetType.DROPDOWN,
-'setting_type': SettingType.SECURE,
-'widget_data': {
-'values': {
-  '0': 'Hide' ,
-  '1': 'Show' ,
+      'default': false ,
+    } ,
+    'version': '3.1.3' ,
+  } ,
+  'status_bar_clock': {
+    'title': 'Hide or show clock on statusbar' ,
+    'icon': Icons.crop_portrait ,
+    'widget': WidgetType.DROPDOWN ,
+    'setting_type': SettingType.SECURE ,
+    'widget_data': {
+      'values': {
+        '0': 'Hide' ,
+        '1': 'Show' ,
       }
-    },
-  'dependencies': [
-    {
-      'name': 'smart_clock_enable',
-      'setting_type': SettingType.SYSTEM,
-      'value': false,
-    },
-  ],
-  'version': '3.1.3',
-  },
+    } ,
+    'dependencies': [
+      {
+        'name': 'smart_clock_enable' ,
+        'setting_type': SettingType.SYSTEM ,
+        'value': false ,
+      } ,
+    ] ,
+    'version': '3.1.3' ,
+  } ,
   'statusbar_clock_am_pm_style': {
-    'title': 'AM/PM style',
-    'icon': Icons.crop_portrait,
-    'widget': WidgetType.DROPDOWN,
-    'setting_type': SettingType.SECURE,
+    'title': 'AM/PM style' ,
+    'icon': Icons.crop_portrait ,
+    'widget': WidgetType.DROPDOWN ,
+    'setting_type': SettingType.SECURE ,
     'widget_data': {
       'values': {
         '0': 'Hide AM/PM' ,
         '1': 'Small AM/PM' ,
         '2': 'Normal AM/PM' ,
       }
-    },
-    'version': '3.1.3',
-  },
+    } ,
+    'version': '3.1.3' ,
+  } ,
   'statusbar_clock_date_display': {
-    'title': 'Date size',
-    'icon': Icons.crop_portrait,
-    'widget': WidgetType.DROPDOWN,
-    'setting_type': SettingType.SECURE,
+    'title': 'Date size' ,
+    'icon': Icons.crop_portrait ,
+    'widget': WidgetType.DROPDOWN ,
+    'setting_type': SettingType.SECURE ,
     'widget_data': {
       'values': {
         '0': 'No date' ,
         '1': 'Small date' ,
         '2': 'Normal date' ,
       }
-    },
-    'version': '3.1.3',
-  },
+    } ,
+    'version': '3.1.3' ,
+  } ,
   'statusbar_clock_date_style': {
-    'title': 'Date style',
-    'icon': Icons.crop_portrait,
-    'widget': WidgetType.DROPDOWN,
-    'setting_type': SettingType.SECURE,
+    'title': 'Date style' ,
+    'icon': Icons.crop_portrait ,
+    'widget': WidgetType.DROPDOWN ,
+    'setting_type': SettingType.SECURE ,
     'widget_data': {
       'values': {
         '0': 'Regular' ,
         '1': 'Lowercase' ,
         '2': 'Uppercase' ,
       }
-    },
-    'version': '3.1.3',
-  },
+    } ,
+    'version': '3.1.3' ,
+  } ,
   'statusbar_clock_date_position': {
-    'title': 'Date position',
-    'icon': Icons.crop_portrait,
-    'widget': WidgetType.DROPDOWN,
-    'setting_type': SettingType.SECURE,
+    'title': 'Date position' ,
+    'icon': Icons.crop_portrait ,
+    'widget': WidgetType.DROPDOWN ,
+    'setting_type': SettingType.SECURE ,
     'widget_data': {
       'values': {
         '0': 'Left of clock' ,
         '1': 'Right of clock' ,
       }
-    },
-    'version': '3.1.3',
-  },
-  'use_old_mobiletype': {
-    'title': 'Old mobile data icons',
-    'subtitle': 'Merge signal and type icons',
+    } ,
+    'version': '3.1.3' ,
+  } ,
+};
+
+
+final Map<String, dynamic> netTraffic = {
+  'network_traffic_state': {
+    'title': 'Network traffic',
+    'subtitle': 'Enable or disable network speed indicators',
     'widget': WidgetType.SWITCH,
     'setting_type': SettingType.SYSTEM,
     'widget_data': {
       'default': false,
     },
+  },
+  'network_traffic_hidearrow': {
+    'title': 'Hide arrows',
+    'subtitle': 'Hide the network traffic indicator arrows',
+    'widget': WidgetType.SWITCH,
+    'setting_type': SettingType.SYSTEM,
+    'widget_data': {
+      'default': false,
+    },
+  },
+  'network_traffic_autohide_threshold': {
+    'title': 'Net activity autohide threshold (KB/s)',
+    'widget': WidgetType.SLIDER,
+    'setting_type': SettingType.SYSTEM,
+    'widget_data': {
+      'default': 0,
+      'min': 0,
+      'max': 10,
+      'percentage': true,
+    },
+    'dependencies': [
+      {
+        'name': 'network_traffic_state',
+        'setting_type': SettingType.SYSTEM,
+        'value': true,
+      },
+    ],
   },
 };
