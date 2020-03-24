480679dc1a97704927395423cc94b6d83fe42c4f
Kshitij Gupta <kshitijgm@gmail.com>
2020-03-25 01:43:04 +0530

    fries: Add Activity launch tile
    Change-Id: I634d78b65444d3841fda80984e59c322e6739bcb

---
+++ lib/widgets/activity.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:potato_fries/app_native/utils.dart';
+import 'package:potato_fries/ui/sizeable_list_tile.dart';
+
+class ActivityTile extends StatelessWidget {
+  final String title;
+  final String subtitle;
+  final String footer;
+  final Widget icon;
+  final bool enabled;
+  final String cls;
+  final String pkg;
+
+  ActivityTile({
+    @required this.title,
+    this.subtitle,
+    this.footer,
+    this.icon,
+    this.enabled = true,
+    @required this.cls,
+    @required this.pkg,
+  })  : assert(title != null),
+        assert(cls != null),
+        assert(pkg != null);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizeableListTile(
+      title: title,
+      icon: icon,
+      subtitle: subtitle == null ? null : Text(subtitle),
+      footer: footer,
+      onTap: () => Utils.startActivity(pkg: pkg, cls: cls),
+    );
+  }
+}
