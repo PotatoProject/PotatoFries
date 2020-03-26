e9802d30feac4ad7f90aa9662492a9ba77bcc7f4
HrX03 <dn.bianco03@gmail.com>
2020-03-26 19:32:23 +0100

    fries: New bottom bar
    Change-Id: I01ff1050b50b02368fafa54cb27afa610b534a66

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
+import 'package:potato_fries/data/constants.dart';
 import 'package:potato_fries/pages/buttons.dart';
 import 'package:potato_fries/pages/lock_screen.dart';
 import 'package:potato_fries/pages/misc.dart';
@@ -6,9 +8,9 @@ import 'package:potato_fries/pages/qs.dart';
 import 'package:potato_fries/pages/status_bar.dart';
 import 'package:potato_fries/pages/themes.dart';
 import 'package:potato_fries/provider/app_info.dart';
-import 'package:potato_fries/ui/custom_bottom_bar.dart';
 import 'package:potato_fries/widgets/directory.dart';
 import 'package:provider/provider.dart';
+import 'package:spicy_components/spicy_components.dart';
 
 class FriesHome extends StatefulWidget {
   @override
@@ -18,6 +20,7 @@ class FriesHome extends StatefulWidget {
 class _FriesHomeState extends State<FriesHome> {
   PageController pageController =
       PageController(initialPage: 0, keepPage: false);
+
   @override
   void initState() {
     registerCustomWidgets();
@@ -28,19 +31,28 @@ class _FriesHomeState extends State<FriesHome> {
   Widget build(BuildContext context) {
     var provider = Provider.of<AppInfoProvider>(context);
     return Scaffold(
-      bottomNavigationBar: CustomBottomBar(
-        currentIndex: provider.pageIndex,
-        onTap: (index) {
-          provider.pageIndex = index;
-          pageController.jumpToPage(provider.pageIndex);
-        },
-        items: [
-          Icon(Icons.swap_vertical_circle),
-          Icon(Icons.touch_app),
-          Icon(Icons.colorize),
-          Icon(Icons.space_bar),
-          Icon(Icons.lock),
-          Icon(Icons.star)
+      bottomNavigationBar: SpicyBottomBar(
+        leftItems: [
+          IconButton(
+            icon: Icon(Icons.menu),
+            padding: EdgeInsets.all(0),
+            onPressed: () {},
+          ),
+          Text(
+            pageLabels[provider.pageIndex],
+            style: TextStyle(
+              fontSize: 18,
+              fontWeight: FontWeight.w500,
+              color: Theme.of(context).iconTheme.color.withOpacity(0.7),
+            ),
+          ),
+        ],
+        rightItems: [
+          IconButton(
+            icon: Icon(MdiIcons.accountGroupOutline),
+            padding: EdgeInsets.all(0),
+            onPressed: () {},
+          ),
         ],
       ),
       body: PageView(
