31b91f3939e8c2965ed77115039fe13683672a3d
HrX03 <dn.bianco03@gmail.com>
2020-03-26 19:32:23 +0100

    fries: New bottom bar
    Change-Id: I01ff1050b50b02368fafa54cb27afa610b534a66

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 import 'package:potato_fries/pages/buttons.dart';
 import 'package:potato_fries/pages/lock_screen.dart';
 import 'package:potato_fries/pages/misc.dart';
@@ -9,6 +10,9 @@ import 'package:potato_fries/provider/app_info.dart';
 import 'package:potato_fries/ui/custom_bottom_bar.dart';
 import 'package:potato_fries/widgets/directory.dart';
 import 'package:provider/provider.dart';
+import 'package:spicy_components/spicy_components.dart';
+
+import '../data/constants.dart';
 
 class FriesHome extends StatefulWidget {
   @override
@@ -28,7 +32,31 @@ class _FriesHomeState extends State<FriesHome> {
   Widget build(BuildContext context) {
     var provider = Provider.of<AppInfoProvider>(context);
     return Scaffold(
-      bottomNavigationBar: CustomBottomBar(
+      bottomNavigationBar: SpicyBottomBar(
+        leftItems: [
+          IconButton(
+            icon: Icon(Icons.menu),
+            padding: EdgeInsets.all(0),
+            onPressed: () {},
+          ),
+          Text(
+            pageLabels[provider.pageIndex],
+            style: TextStyle(
+              fontSize: 18,
+              fontWeight: FontWeight.w500,
+              color: Theme.of(context).iconTheme.color.withOpacity(0.7),
+            ),
+          ),
+        ],
+        rightItems: [
+          IconButton(
+            icon: Icon(MdiIcons.accountGroupOutline),
+            padding: EdgeInsets.all(0),
+            onPressed: () {},
+          ),
+        ],
+      ),
+      /*bottomNavigationBar: CustomBottomBar(
         currentIndex: provider.pageIndex,
         onTap: (index) {
           provider.pageIndex = index;
@@ -42,7 +70,7 @@ class _FriesHomeState extends State<FriesHome> {
           Icon(Icons.lock),
           Icon(Icons.star)
         ],
-      ),
+      ),*/
       body: PageView(
         controller: pageController,
         physics: NeverScrollableScrollPhysics(),
