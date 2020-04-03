b09ffc97a83eaaabdf285592a933f0d53864a6ac
rajadeja <rajsjadeja4@gmail.com>
2020-04-03 22:59:46 +0530

    Add battery & clock customizations
    Change-Id: Ifad59a21059460c9b9936397304f21cd1f096496

--- lib/data/status_bar.dart
+++ lib/data/status_bar.dart
@@ -6,20 +6,11 @@ final Map<String, dynamic> statusBar = {
   'StatusBar Icons': statusbaricons,
   'Network Traffic Monitor': netTraffic,
   'Display cutouts': displayCutouts,
+  'Battery': statusbarbattery,
+  'Clock': statusbarclock,
 };
 
 final Map<String, dynamic> statusbaricons = {
-  'smart_clock_enable': {
-    'title': 'Smart Clock',
-    'subtitle': 'Show statusbar clock periodically',
-    'icon': Icons.fiber_smart_record,
-    'widget': WidgetType.SWITCH,
-    'setting_type': SettingType.SYSTEM,
-    'widget_data': {
-      'default': false,
-    },
-    'version': '3.1.3',
-  },
   'status_bar_logo': {
     'title': 'POSP swag',
     'subtitle': 'Show off POSP logo on statusbar',
@@ -159,6 +150,132 @@ final Map<String, dynamic> displayCutouts = {
         'value': '2',
       },
     ],
+    'version': '3.1.5',
+  },
+};
+
+final Map<String, dynamic> statusbarbattery = {
+  'status_bar_battery_style': {
+    'title': 'Battery Style',
+    'icon': Icons.crop_portrait,
+    'widget': WidgetType.DROPDOWN,
+    'setting_type': SettingType.SYSTEM,
+    'widget_data': {
+      'values': {
+        '0': 'Portrait' ,
+        '1': 'Circle' ,
+        '2': 'Dotted Circle' ,
+        '3': 'Solid Circle' ,
+        '4': 'Hidden' ,
+      },
+      'default': '0',
+    },
+  },
+  'status_bar_show_battery_percent': {
+    'title': 'Battery Percentage',
+    'icon': Icons.crop_portrait,
+    'widget': WidgetType.DROPDOWN,
+    'setting_type': SettingType.SYSTEM,
+    'widget_data': {
+      'values': {
+        '0': 'Disabled' ,
+        '1': 'Enabled' ,
+        '2': 'Enabled while charging' ,
+      },
+      'default': '0',
+    },
+  },
+};
+final Map<String, dynamic> statusbarclock = {
+  'smart_clock_enable': {
+    'title': 'Smart Clock',
+    'subtitle': 'Show statusbar clock periodically',
+    'icon': Icons.fiber_smart_record,
+    'widget': WidgetType.SWITCH,
+    'setting_type': SettingType.SYSTEM,
+    'widget_data': {
+      'default': false,
+    },
+    'version': '3.1.3',
+  },
+  'status_bar_clock': {
+    'title': 'Hide or show clock on statusbar',
+    'icon': Icons.crop_portrait,
+    'widget': WidgetType.DROPDOWN,
+    'setting_type': SettingType.SECURE,
+    'widget_data': {
+      'values': {
+        '0': 'Hide' ,
+        '1': 'Show' ,
+      },
+      'default': '1'
+    },
+    'dependencies': [
+      {
+        'name': 'smart_clock_enable',
+        'setting_type': SettingType.SYSTEM,
+        'value': false,
+      },
+    ],
+    'version': '3.1.5',
+  },
+  'statusbar_clock_am_pm_style': {
+    'title': 'AM/PM style',
+    'icon': Icons.crop_portrait,
+    'widget': WidgetType.DROPDOWN,
+    'setting_type': SettingType.SECURE,
+    'widget_data': {
+      'values': {
+        '0': 'Hide AM/PM' ,
+        '1': 'Small AM/PM' ,
+        '2': 'Normal AM/PM' ,
+      },
+      'default': '0',
+    },
+    'version': '3.1.5',
+  },
+  'statusbar_clock_date_display': {
+    'title': 'Date size',
+    'icon': Icons.crop_portrait,
+    'widget': WidgetType.DROPDOWN,
+    'setting_type': SettingType.SECURE,
+    'widget_data': {
+      'values': {
+        '0': 'No date' ,
+        '1': 'Small date' ,
+        '2': 'Normal date' ,
+      },
+      'default': '0',
+    },
+    'version': '3.1.5',
+  },
+  'statusbar_clock_date_style': {
+    'title': 'Date style',
+    'icon': Icons.crop_portrait,
+    'widget': WidgetType.DROPDOWN,
+    'setting_type': SettingType.SECURE,
+    'widget_data': {
+      'values': {
+        '0': 'Regular' ,
+        '1': 'Lowercase' ,
+        '2': 'Uppercase' ,
+      },
+      'default': '0',
+    },
+    'version': '3.1.5',
+  },
+  'statusbar_clock_date_position': {
+    'title': 'Date position',
+    'icon': Icons.crop_portrait,
+    'widget': WidgetType.DROPDOWN,
+    'setting_type': SettingType.SECURE,
+    'widget_data': {
+      'values': {
+        '0': 'Left of clock' ,
+        '1': 'Right of clock' ,
+      },
+      'default': '0',
+    },
     'version': '3.1.5',
   },
 };
