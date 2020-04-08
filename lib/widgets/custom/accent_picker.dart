21422e21b633c23021c07b1c7e8a5dd927aa8908
Kshitij Gupta <kshitijgm@gmail.com>
2020-04-08 23:12:39 +0530

    fries: Debug menu
    Change-Id: I4ad34f1d670e354d99c0b69f0d23b00d7d42c049

--- lib/widgets/custom/accent_picker.dart
+++ lib/widgets/custom/accent_picker.dart
@@ -1,7 +1,6 @@
 import 'package:android_flutter_settings/android_flutter_settings.dart';
 import 'package:flutter/material.dart';
 import 'package:potato_fries/provider/app_info.dart';
-import 'package:potato_fries/utils/methods.dart';
 import 'package:potato_fries/widgets/color_picker_dual.dart';
 import 'package:provider/provider.dart';
 
@@ -25,14 +24,10 @@ class AccentPicker extends StatelessWidget {
         lightnessDeltaCenter: 0.15,
         lightnessDeltaEnd: 0.6,
         onApply: (String newDark, String newLight) {
-          var hostVer =
-              Provider.of<AppInfoProvider>(context, listen: false).hostVersion;
           var settingImplVer = '3.1.1';
-          var settingImpl = isVersionCompatible(
-            settingImplVer,
-            hostVer,
-            strict: true,
-          );
+          bool settingImpl =
+              Provider.of<AppInfoProvider>(context, listen: false)
+                  .isCompatible(settingImplVer, strict: true);
           if (!settingImpl) {
             AndroidFlutterSettings.setProp(
               'persist.sys.theme.accent_dark',
