a6e6f028fe6a495139c38b8a695a895c35560fbc
HrX03 <dn.bianco03@gmail.com>
2020-05-05 11:29:06 +0200

    fries: Convert sliders into stepped sliders\
    Change-Id: Ic2ccb17504840c727380578a39280d49deda7bc7

--- lib/widgets/settings_dropdown.dart
+++ lib/widgets/settings_dropdown.dart
@@ -41,26 +41,24 @@ class _SettingsDropdownTileState extends State<SettingsDropdownTile> {
   Widget build(BuildContext context) {
     value = widget.getValue() ?? widget.defaultValue ?? widget.values[0];
     return SizeableListTile(
-      height: 64,
       title: widget.title,
-      subtitle: widget.subtitle != null ? Text(widget.subtitle) : null,
-      icon: widget.icon,
-      trailing: Text(
+      subtitle: Text(
         widget.values[value],
         style: TextStyle(
             color: Theme.of(context).textTheme.title.color.withAlpha(160)),
       ),
+      icon: widget.icon,
       onTap: () async {
         var result = await showModalBottomSheet(
             context: context,
             isScrollControlled: false,
             builder: (context) => ListView(
                   shrinkWrap: true,
+                  physics: ClampingScrollPhysics(),
                   children: List.generate(widget.values.length, (index) {
                     bool selected = widget.values.values.toList()[index] ==
                         widget.values[value];
                     return SizeableListTile(
-                      height: 56,
                       title: widget.values.values.toList()[index],
                       icon: selected ? Icon(Icons.check) : null,
                       selected: selected,
@@ -74,48 +72,6 @@ class _SettingsDropdownTileState extends State<SettingsDropdownTile> {
           widget.setValue(result);
         }
       },
-      /*title: Container(
-        width: double.maxFinite,
-        child: DropdownButton(
-          value: value,
-          items: widget.values.keys
-              .map(
-                (k) => DropdownMenuItem(
-                  value: k,
-                  child: Text(widget.values[k]),
-                ),
-              )
-              .toList(),
-          onChanged: (_value) {
-            setState(() => value = _value);
-            widget.setValue(value);
-          },
-        ),
-      ),
-      icon: widget.icon,
-      subtitle: Builder(
-        builder: (context) {
-          if (widget.subtitle == null && widget.title == null) return null;
-          if (widget.subtitle == null)
-            return Text(widget.title,
-                style: TextStyle(
-                    color: Theme.of(context).brightness == Brightness.dark
-                        ? Colors.white
-                        : Colors.black));
-          else
-            return CrossFadePeriodic(
-              firstChild: Text(
-                widget.title,
-                style: TextStyle(
-                    color: Theme.of(context).brightness == Brightness.dark
-                        ? Colors.white
-                        : Colors.black),
-              ),
-              secondChild: Text(widget.subtitle),
-            );
-        },
-      ),
-      footer: widget.footer,*/
     );
   }
 }
