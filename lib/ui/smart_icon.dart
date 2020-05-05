3845ed17d0f614d1c916787826c8a0fd7cda7275
HrX03 <dn.bianco03@gmail.com>
2020-05-05 15:14:51 +0200

    fries: Add support for svg and xml icons
    Change-Id: I996fac9068a4ca6b61d48c6706a7a82d4c8fcd7a

---
+++ lib/ui/smart_icon.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/avd.dart';
+import 'package:flutter_svg/svg.dart';
+
+class SmartIcon extends StatelessWidget {
+  final dynamic iconData;
+  final double size;
+  final Color color;
+
+  SmartIcon(
+    this.iconData, {
+    this.size,
+    this.color,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    var _color = color ?? Theme.of(context).brightness == Brightness.light
+        ? Colors.black
+        : Colors.white;
+    if (iconData is IconData) {
+      return Icon(
+        iconData,
+        size: size,
+        color: _color,
+      );
+    } else if (iconData is String) {
+      if ((iconData as String).endsWith("svg")) {
+        return SvgPicture.asset(
+          iconData,
+          height: size ?? 24,
+          width: size ?? 24,
+          color: _color,
+        );
+      } else if ((iconData as String).endsWith("xml")) {
+        return SizedBox.fromSize(
+          size: Size.square(size ?? 24),
+          child: AvdPicture.asset(
+            iconData,
+            color: _color,
+          ),
+        );
+      } else
+        throw TypeError();
+    } else if (iconData == null) {
+      return SizedBox.fromSize(
+        size: Size.square(size ?? 24),
+      );
+    } else {
+      throw TypeError();
+    }
+  }
+}
