6045dd1cdc40d70660f359a514259c25bddec623
HrX03 <dn.bianco03@gmail.com>
2020-05-06 11:41:47 +0200

    fries: Commonize provider and abstract page
    Change-Id: I649291fb2ab863fdea00f47d0152960b09569779

--- lib/pages/buttons.dart
+++ lib/pages/buttons.dart
@@ -1,30 +1,32 @@
 import 'package:flutter/material.dart';
-import 'package:potato_fries/provider/buttons.dart';
+import 'package:outline_material_icons/outline_material_icons.dart';
+import 'package:potato_fries/pages/page.dart';
+import 'package:potato_fries/provider/page_provider_registry.dart';
 import 'package:potato_fries/widgets/page_parser.dart';
 import 'package:provider/provider.dart';
 
-class Buttons extends StatelessWidget {
-  final ButtonsDataProvider provider = ButtonsDataProvider();
+class Buttons extends Page {
+  @override
+  String get title => "Gestures";
+
+  @override
+  IconData get icon => OMIcons.touchApp;
+
+  @override
+  String get providerKey => 'buttons_and_gestures';
 
   @override
   Widget build(BuildContext context) {
     return ChangeNotifierProvider.value(
-      value: provider,
-      child: _ButtonsBody(),
+      value: PageProviderRegistry.getProvider(providerKey),
+      child: Builder(
+        builder: (providerContext) => Column(
+          children: <Widget>[
+            Container(),
+            PageParser(dataKey: providerKey),
+          ],
+        ),
+      ),
     );
   }
-}
-
-class _ButtonsBody extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        _header(context),
-        PageParser(dataKey: 'buttons_and_gestures'),
-      ],
-    );
-  }
-
-  Widget _header(BuildContext context) => Container();
 }
