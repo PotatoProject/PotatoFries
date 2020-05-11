1acbae53fd7168fd19e35c5dde935ecc0aa6682c
rajadeja <rajsjadeja4@gmail.com>
2020-05-11 15:11:15 +0530

    Hello themes
    Change-Id: Ia570e65eaddd8c9217eb79b8c2e9e81bf371af6b

--- lib/data/themes.dart
+++ lib/data/themes.dart
@@ -1,7 +1,10 @@
 import 'package:potato_fries/widgets/directory.dart';
+import 'package:android_flutter_settings/android_flutter_settings.dart';
+import 'package:flutter/material.dart';
 
 final Map<String, dynamic> themeData = {
   'Accent': themeAccent,
+  'Themes': theme,
 };
 
 final Map<String, dynamic> themeAccent = {
@@ -17,4 +20,22 @@ final Map<String, dynamic> themeAccent = {
     'widget': WidgetType.CUSTOM,
     'setting_type': 'IconPackPicker',
   },
+};
+
+final Map<String, dynamic> theme = {
+'color_bucket_overlay': {
+  'title': 'Theme' ,
+  'icon': Icons.format_color_text ,
+  'widget': WidgetType.DROPDOWN ,
+  'setting_type': SettingType.SYSTEM ,
+  'widget_data': {
+    'values': {
+      'package_device_default': 'Default theme',
+      'com.android.dark.darkgray': 'Dark',
+      'com.android.dark.night': 'Night',
+      'com.android.dark.style': 'Style',
+    },
+'default':'package_device_default',
+  },
+},
 };
