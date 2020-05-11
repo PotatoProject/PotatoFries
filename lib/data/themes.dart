f782122f52a4e42d744bdc69488d53fb1f0ced4f
rajadeja <rajsjadeja4@gmail.com>
2020-05-11 15:11:15 +0530

    fries: Introduce themes and upgrade deps
    Change-Id: Ia570e65eaddd8c9217eb79b8c2e9e81bf371af6b

--- lib/data/themes.dart
+++ lib/data/themes.dart
@@ -1,10 +1,12 @@
+import 'package:flutter/material.dart';
 import 'package:potato_fries/widgets/directory.dart';
+import 'package:android_flutter_settings/android_flutter_settings.dart';
 
 final Map<String, dynamic> themeData = {
-  'Accent': themeAccent,
+  'Themes': theme,
 };
 
-final Map<String, dynamic> themeAccent = {
+final Map<String, dynamic> theme = {
   'system_accent': {
     'widget': WidgetType.CUSTOM,
     'setting_type': 'AccentPicker',
@@ -16,5 +18,21 @@ final Map<String, dynamic> themeAccent = {
   'system_icon_pack': {
     'widget': WidgetType.CUSTOM,
     'setting_type': 'IconPackPicker',
+  },
+  'color_bucket_overlay': {
+    'title': 'Theme mode',
+    'icon': Icons.color_lens,
+    'widget': WidgetType.DROPDOWN,
+    'setting_type': SettingType.SYSTEM,
+    'widget_data': {
+      'values': {
+        'package_device_default': 'Default theme',
+        'com.android.dark.darkgray': 'Dark',
+        'com.android.dark.night': 'Night',
+        'com.android.dark.style': 'Style',
+      },
+      'default': 'package_device_default',
+    },
+    'version': '3.1.8',
   },
 };
