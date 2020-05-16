ec38eb87aa2ed9b4fb56b0c384f6b1707e55b0ee
Kshitij Gupta <kshitijgm@gmail.com>
2020-05-16 19:53:59 +0530

    fries: Allow extending providers, add them to registry
    Change-Id: Ib91d66ddf51a132efccda7d436fa1e73505e58ef

--- lib/provider/page_provider_registry.dart
+++ lib/provider/page_provider_registry.dart
@@ -1,6 +1,19 @@
+import 'package:potato_fries/provider/buttons.dart';
+import 'package:potato_fries/provider/lock_screen.dart';
+import 'package:potato_fries/provider/misc.dart';
 import 'package:potato_fries/provider/page_provider.dart';
+import 'package:potato_fries/provider/qs.dart';
+import 'package:potato_fries/provider/status_bar.dart';
+import 'package:potato_fries/provider/themes.dart';
 
-Map<String, PageProvider> _registry = {};
+Map<String, PageProvider> _registry = {
+  ButtonsProvider.buttonsProviderKey: ButtonsProvider(),
+  LockScreenProvider.lockScreenProviderKey: LockScreenProvider(),
+  MiscProvider.miscProviderKey: MiscProvider(),
+  QSProvider.qsProviderKey: QSProvider(),
+  StatusBarProvider.statusBarProviderKey: StatusBarProvider(),
+  ThemesProvider.themesProviderKey: ThemesProvider(),
+};
 
 class PageProviderRegistry {
   static PageProvider getProvider(String key) {
@@ -10,9 +23,7 @@ class PageProviderRegistry {
       return provider;
     } else {
       PageProvider newProvider = PageProvider(key);
-      _registry.addAll({
-        key: newProvider,
-      });
+      _registry[key] = newProvider;
       return newProvider;
     }
   }
