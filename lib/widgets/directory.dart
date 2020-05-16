38abfd84b20d69adb2e0e444236ca7ff7fe065ce
Kshitij Gupta <kshitijgm@gmail.com>
2020-05-16 20:36:56 +0530

    fries: themes: Add lockscreen clock
    - Added in themes for now, since it is a remake of the AOSP
      ThemePicker and is with other AOSP options this way
    
    Change-Id: I9ee530d613576159d46365c6476cbe1f5b1a824c

--- lib/widgets/directory.dart
+++ lib/widgets/directory.dart
@@ -2,6 +2,7 @@ import 'package:potato_fries/utils/obj_gen.dart';
 import 'package:potato_fries/widgets/custom/accent_picker.dart';
 import 'package:potato_fries/widgets/custom/icon_pack_picker.dart';
 import 'package:potato_fries/widgets/custom/icon_shape_picker.dart';
+import 'package:potato_fries/widgets/custom/lock_screen_clock_picker.dart';
 
 enum WidgetType {
   COLOR_PICKER,
@@ -17,4 +18,5 @@ void registerCustomWidgets() {
   ObjectGen.register<AccentPicker>(() => AccentPicker());
   ObjectGen.register<IconPackPicker>(() => IconPackPicker());
   ObjectGen.register<IconShapePicker>(() => IconShapePicker());
+  ObjectGen.register<LockScreenClockPicker>(() => LockScreenClockPicker());
 }
