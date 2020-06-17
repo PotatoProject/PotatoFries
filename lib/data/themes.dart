8c89856048c75237d6241e92e05c44176a251ee4
HrX03 <dn.bianco03@gmail.com>
2020-06-17 12:16:26 +0200

    fries: Fix some oopsies
    Change-Id: I6172d21303dc5d2515f3d6407156a2c8ff63daec

--- lib/data/themes.dart
+++ lib/data/themes.dart
@@ -35,7 +35,7 @@ final Map<String, dynamic> theme = {
     },
     'version': '3.1.8',
   },
-  'sysui_volume_plugin': {
+  'systemui_plugin_volume': {
     'title': 'Volume panel',
     'icon': Icons.music_note,
     'widget': WidgetType.DROPDOWN,
@@ -43,13 +43,13 @@ final Map<String, dynamic> theme = {
     'widget_data': {
       'values': {
         'co.potatoproject.plugin.volume.aosp': 'Aosp',
-        'co.potatoproject.plugin.volume.fancy': 'Fancy',
-        'co.potatoproject.plugin.volume.toggle': 'Toggle',
+        'co.potatoproject.plugin.volume.compact': 'Compact',
         'co.potatoproject.plugin.volume.oreo': 'Oreo',
+        'co.potatoproject.plugin.volume.tiled': 'Tiled',
       },
       'default': 'co.potatoproject.plugin.volume.aosp',
     },
-    'version': '3.2.0',
+    'version': '3.1.9',
     'cooldown': 1500,
   },
 };
