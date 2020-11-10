e7aabe9899044e862de56f9b2b73635aadb715db
Raj Jadeja <rajsjadeja4@gmail.com>
2020-11-10 12:28:59 +0530

    fries: Initial changes for R
    Change-Id: I8739a441b70ba812449590d553673b461c8410dd

--- lib/data/buttons.dart
+++ lib/data/buttons.dart
@@ -6,178 +6,43 @@ import 'package:potato_fries/widgets/directory.dart';
 final Map<String, dynamic> buttons = {
   'System Buttons': sysbutton,
   'System Gestures': systemgesture,
-  'Navigation': navigation,
-  'Power Menu Configuration': powermenu,
-  'Navigation Bar': navkeys,
-};
-
-final Map<String, dynamic> sysbutton = {
-  'volume_button_music_control': {
-    'title': 'Volume button to skip tracks',
-    'subtitle': 'Long press volume buttons to forward/backward track',
-    'icon': Icons.music_note,
-    'widget': WidgetType.SWITCH,
-    'setting_type': SettingType.SYSTEM,
-    'widget_data': {
-      'default': false,
-    },
-    'version': '3.1.1',
-  },
-};
-
-final Map<String, dynamic> navigation = {
-  'sysui_nav_bar_hint': {
-    'title': 'Show gestural pill',
-    'subtitle': 'Toggle full screen navigation handle/pill',
-    'icon': MdiIcons.gestureSwipeUp,
-    'widget': WidgetType.SWITCH,
-    'setting_type': SettingType.SECURE,
-    'widget_data': {
-      'default': true,
-    },
-    'version': '3.2.0',
-  },
-  'navigation_handle_width': {
-    'title': 'Navigation handle length',
-    'widget': WidgetType.DROPDOWN,
-    'setting_type': SettingType.SYSTEM,
-    'widget_data': {
-      'values': {
-        '0': 'Stock',
-        '1': 'Medium',
-        '2': 'Long',
-      },
-      'default': '0',
-    },
-    'dependencies': [
-      {
-        'name': 'sysui_nav_bar_hint',
-        'setting_type': SettingType.SECURE,
-        'value': true,
-      },
-    ],
-    'version': '3.2.0',
-  },
-  'sysui_nav_bar_inverse': {
-    'title': 'Switch navigation buttons',
-    'subtitle': 'Swap navigation buttons when using 3 button layout',
-    'icon': MdiIcons.swapHorizontal,
-    'widget': WidgetType.SWITCH,
-    'setting_type': SettingType.SECURE,
-    'widget_data': {
-      'default': false,
-    },
-    'version': '3.2.0',
-  },
-};
-
-final Map<String, dynamic> powermenu = {
-  'reboot_in_power_menu': {
-    'title': 'Restart',
-    'subtitle': 'Add restart in powermenu',
-    'icon': MdiIcons.restart,
-    'widget': WidgetType.SWITCH,
-    'setting_type': SettingType.SECURE,
-    'widget_data': {
-      'default': true,
-    },
-    'version': '3.1.1',
-  },
-  'advanced_reboot_in_power_menu': {
-    'title': 'Advanced restart',
-    'subtitle': 'Add advanced restart in powermenu',
-    'icon': Icons.data_usage,
-    'widget': WidgetType.SWITCH,
-    'setting_type': SettingType.SECURE,
-    'widget_data': {
-      'default': false,
-    },
-    'version': '3.1.1',
-  },
-  'screenshot_in_power_menu': {
-    'title': 'Screenshot menu',
-    'subtitle': 'Add screenshot in powermenu',
-    'icon': MdiIcons.cellphoneScreenshot,
-    'widget': WidgetType.SWITCH,
-    'setting_type': SettingType.SECURE,
-    'widget_data': {
-      'default': true,
-    },
-    'version': '3.1.1',
-  },
-  'screenrecord_in_power_menu': {
-    'title': 'Screenrecord menu',
-    'subtitle': 'Add screenrecord in powermenu',
-    'icon': MdiIcons.video,
-    'widget': WidgetType.SWITCH,
-    'setting_type': SettingType.SECURE,
-    'widget_data': {
-      'default': false,
-    },
-    'version': '3.1.1',
-  },
-  'lockdown_in_power_menu': {
-    'title': 'Lockdown menu',
-    'subtitle': 'Add lockdown in powermenu',
-    'icon': Icons.lock_outline,
-    'widget': WidgetType.SWITCH,
-    'setting_type': SettingType.SECURE,
-    'widget_data': {
-      'default': false,
-    },
-    'version': '3.1.1',
-  },
-  'lockscreen_enable_power_menu': {
-    'title': 'Power menu on Lockscreen',
-    'subtitle': 'Allow accessing power menu on secure lock screen',
-    'icon': Icons.phonelink_lock,
-    'widget': WidgetType.SWITCH,
-    'setting_type': SettingType.GLOBAL,
-    'widget_data': {
-      'default': true,
-    },
-    'version': '3.1.1',
-  },
 };
 
 final Map<String, dynamic> systemgesture = {
-  'double_tap_sleep_gesture': {
-    'title': 'Double tap Status bar to sleep',
-    'subtitle': 'Tap twice on Status bar to put device in sleep',
-    'icon': Icons.touch_app,
+  'double_tap_sleep_lockscreen': {
+    'title': 'Double tap to sleep on losckreen',
+    'subtitle': 'Double tap on lockscreen empty areas to switch the screen off',
+    'icon': Icons.touch_app_outlined,
     'widget': WidgetType.SWITCH,
-    'setting_type': SettingType.SYSTEM,
+    'setting_type': SettingType.SECURE,
     'widget_data': {
       'default': false,
     },
-    'version': '3.1.1',
+    'version': '4.0.0',
   },
-  'three_finger_gesture': {
-    'title': 'Three finger screenshot',
-    'subtitle': 'Swipe down with three fingers to screenshot',
-    'icon': Icons.camera,
+  'pulse_on_new_tracks': {
+    'title': 'Show Ambient Display when a new music track is played. Double tap on track info to skip to next track',
+    'subtitle': 'Show Ambient Display when a new music track is played',
+    'icon': Icons.music_video_outlined,
     'widget': WidgetType.SWITCH,
     'setting_type': SettingType.SYSTEM,
     'widget_data': {
       'default': false,
     },
-    'version': '3.1.7',
+    'version': '4.0.0',
   },
 };
 
-final Map<String, dynamic> navkeys = {
-  'navigation_bar_show': {
-    'title': 'Navigation bar',
-    'subtitle': 'Show system software navigation keys',
-    'icon': Icons.compare_arrows,
-    'widget': WidgetType.SWITCH,
-    'setting_type': SettingType.SYSTEM,
-    'widget_data': {
-      'default': true,
-    },
-    'compat': {
-      'prop': 'ro.potato.has_hwkeys',
-    },
-    'version': '3.1.9',
-  },
+final Map<String, dynamic> sysbutton = {
+'volume_button_music_control': {
+'title': 'Volume button to skip tracks',
+'subtitle': 'Long press volume buttons to forward/backward track',
+'icon': Icons.music_note,
+'widget': WidgetType.SWITCH,
+'setting_type': SettingType.SYSTEM,
+'widget_data': {
+'default': false,
+},
+'version': '4.0.0',
+},
 };
