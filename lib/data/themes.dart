7313525f156b8797625ba3e339041da051f5d125
Raj Jadeja <rajsjadeja4@gmail.com>
2020-11-10 12:28:59 +0530

    fries: Initial changes for R

--- lib/data/themes.dart
+++ lib/data/themes.dart
@@ -7,10 +7,6 @@ final Map<String, dynamic> themeData = {
 };
 
 final Map<String, dynamic> theme = {
-  'system_accent': {
-    'widget': WidgetType.CUSTOM,
-    'setting_type': 'AccentPicker',
-  },
   'system_icon_shape': {
     'widget': WidgetType.CUSTOM,
     'setting_type': 'IconShapePicker',
@@ -18,38 +14,5 @@ final Map<String, dynamic> theme = {
   'system_icon_pack': {
     'widget': WidgetType.CUSTOM,
     'setting_type': 'IconPackPicker',
-  },
-  'color_bucket_overlay': {
-    'title': 'Theme mode',
-    'icon': Icons.color_lens,
-    'widget': WidgetType.DROPDOWN,
-    'setting_type': SettingType.SYSTEM,
-    'widget_data': {
-      'values': {
-        'package_device_default': 'Default theme',
-        'com.android.dark.darkgray': 'Dark',
-        'com.android.dark.night': 'Night',
-        'com.android.dark.style': 'Style',
-      },
-      'default': 'package_device_default',
-    },
-    'version': '3.1.8',
-  },
-  'systemui_plugin_volume': {
-    'title': 'Volume panel',
-    'icon': Icons.music_note,
-    'widget': WidgetType.DROPDOWN,
-    'setting_type': SettingType.SYSTEM,
-    'widget_data': {
-      'values': {
-        'co.potatoproject.plugin.volume.aosp': 'AOSP',
-        'co.potatoproject.plugin.volume.compact': 'Compact',
-        'co.potatoproject.plugin.volume.oreo': 'Oreo',
-        'co.potatoproject.plugin.volume.tiled': 'Tiled',
-      },
-      'default': 'co.potatoproject.plugin.volume.aosp',
-    },
-    'version': '3.2.0',
-    'cooldown': 1500,
   },
 };
