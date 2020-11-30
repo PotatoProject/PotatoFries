71d99ca20a513d0a7c313e69d24857e8a9d0c91a
HrX03 <dn.bianco03@gmail.com>
2020-11-30 09:17:14 +0100

    fries: Implement icon pack picker
    Change-Id: I40a6f90ae457a9de1598b18a64786f8eb931b209

--- lib/widgets/custom/icon_pack_picker.dart
+++ lib/widgets/custom/icon_pack_picker.dart
@@ -1,25 +1,93 @@
 import 'package:flutter/material.dart';
-import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 import 'package:potato_fries/data/constants.dart';
 import 'package:potato_fries/provider/app_info.dart';
-import 'package:potato_fries/widgets/settings_dropdown.dart';
+import 'package:potato_fries/ui/sizeable_list_tile.dart';
 import 'package:provider/provider.dart';
 
 class IconPackPicker extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return SettingsDropdownTile(
-      title: 'System icon Pack',
-      subtitle: 'Pick system icon pack',
-      icon: Icon(MdiIcons.wifiStrength3),
-      setValue: (val) => Provider.of<AppInfoProvider>(context, listen: false)
-          .setIconPack(int.parse(val)),
-      getValue: () =>
-          Provider.of<AppInfoProvider>(context).getIconPackIndex().toString(),
-      values: Map.fromIterable(
-        iconPackLabels,
-        key: (element) => iconPackLabels.indexOf(element).toString(),
-        value: (element) => element,
+    final appInfo = Provider.of<AppInfoProvider>(context);
+    final icons = appInfo.iconPreviews;
+    final selectedPack = appInfo.globalSysTheme[OVERLAY_CATEGORY_ICON_ANDROID];
+
+    return SizeableListTile(
+      title: 'System icon pack',
+      onTap: () {
+        showModalBottomSheet(
+          context: context,
+          builder: (context) {
+            return Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.only(
+                    top: 24,
+                    left: 24,
+                    bottom: 24,
+                  ),
+                  child: Text(
+                    'System icon pack',
+                    style: Theme.of(context).textTheme.headline6,
+                  ),
+                ),
+                GridView.builder(
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 4,
+                  ),
+                  padding: EdgeInsets.zero,
+                  itemBuilder: (context, index) {
+                    final MapEntry iconInfo = MapEntry(
+                      icons.keys.toList()[index],
+                      icons.values.toList()[index]["ic_wifi_signal_3"],
+                    );
+                    final selected = selectedPack == iconInfo.key;
+
+                    return Center(
+                      child: GestureDetector(
+                        onTap: () {
+                          Navigator.pop(context);
+                          appInfo.setIconPack(index);
+                        },
+                        child: Image.memory(
+                          iconInfo.value,
+                          width: 36,
+                          height: 36,
+                          color: selected
+                              ? Theme.of(context).accentColor
+                              : Theme.of(context).iconTheme.color,
+                          colorBlendMode: BlendMode.srcIn,
+                        ),
+                      ),
+                    );
+                  },
+                  itemCount: icons.length,
+                  shrinkWrap: true,
+                ),
+              ],
+            );
+          },
+        );
+      },
+      icon: Image.memory(
+        appInfo.getIconPackPreview()["ic_wifi_signal_3"],
+        width: 24,
+        height: 24,
+        color: Theme.of(context).brightness == Brightness.light
+            ? Colors.black
+            : Colors.white,
+        colorBlendMode: BlendMode.srcIn,
+      ),
+      subtitle: AnimatedSwitcher(
+        duration: Duration(milliseconds: 300),
+        child: Text(appInfo.getIconPackLabel()),
+        switchInCurve: Curves.easeInOut,
+        switchOutCurve: Curves.easeInOut,
+        transitionBuilder: (child, animation) => FadeTransition(
+          opacity: animation,
+          child: child,
+        ),
       ),
     );
   }
