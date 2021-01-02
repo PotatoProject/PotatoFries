7ab2c3043bf14427db6c0a2cece139981d56d73a
HrX03 <dn.bianco03@gmail.com>
2021-01-02 23:51:54 +0100

    fries: Fix some problems reported by IDE
    Change-Id: Ia852e80e72d9538240be1f00535cda09d1cba67e

--- lib/data/system.dart
+++ lib/data/system.dart
@@ -1,7 +1,6 @@
 import 'package:android_flutter_settings/android_flutter_settings.dart';
 import 'package:flutter/material.dart';
 import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
-import 'package:outline_material_icons/outline_material_icons.dart';
 import 'package:potato_fries/locales/locale_strings.g.dart';
 import 'package:potato_fries/widgets/directory.dart';
 
