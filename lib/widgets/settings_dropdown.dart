3b049e4a93ae4c2fa01c0a51412f59d1f129dcc4
HrX03 <dn.bianco03@gmail.com>
2021-01-09 20:29:35 +0100

    fries: Deprecate cooldown for tiles
    We don't use it and we don't plan to, so it's useless to keep it
    
    Change-Id: I16aa7550c2881c2ed66c140f05342e9dbe13ef66

--- lib/widgets/settings_dropdown.dart
+++ lib/widgets/settings_dropdown.dart
@@ -6,14 +6,12 @@ import 'package:potato_fries/ui/smart_icon.dart';
 import 'package:provider/provider.dart';
 
 class SettingsDropdownTile extends StatefulWidget {
-  final bool enabled;
   final SettingPreference pref;
-  final int cooldown;
+  final bool enabled;
 
   SettingsDropdownTile({
     @required this.pref,
     this.enabled = true,
-    this.cooldown,
   }) : assert(pref != null);
 
   @override
@@ -21,8 +19,6 @@ class SettingsDropdownTile extends StatefulWidget {
 }
 
 class _SettingsDropdownTileState extends State<SettingsDropdownTile> {
-  bool coolingDown = false;
-
   @override
   Widget build(BuildContext context) {
     final _provider = context.watch<PageProvider>();
@@ -49,7 +45,6 @@ class DropdownTile extends StatefulWidget {
   final String value;
   final ValueChanged<String> onValueChanged;
   final String defaultValue;
-  final int cooldown;
   final Color selectedColor;
 
   DropdownTile({
@@ -60,7 +55,6 @@ class DropdownTile extends StatefulWidget {
     @required this.value,
     this.onValueChanged,
     this.defaultValue,
-    this.cooldown,
     this.selectedColor,
   });
 
@@ -69,65 +63,49 @@ class DropdownTile extends StatefulWidget {
 }
 
 class _DropdownTileState extends State<DropdownTile> {
-  bool coolingDown = false;
-
   @override
   Widget build(BuildContext context) {
     final value =
         widget.value ?? widget.defaultValue ?? widget.values.values.first;
 
-    return AnimatedOpacity(
-      opacity: coolingDown ? 0.5 : 1,
-      duration: Duration(milliseconds: 300),
-      child: IgnorePointer(
-        ignoring: coolingDown,
-        child: SizeableListTile(
-          title: widget.title,
-          subtitle: Text(
-            widget.values[value],
-            style: TextStyle(
-              color: Theme.of(context).textTheme.headline6.color.withAlpha(160),
-            ),
-          ),
-          selectedColor: widget.selectedColor,
-          icon: widget.icon,
-          onTap: () async {
-            var result = await showModalBottomSheet(
-              context: context,
-              isScrollControlled: false,
-              builder: (context) => ListView(
-                shrinkWrap: true,
-                physics: ClampingScrollPhysics(),
-                children: List.generate(
-                  widget.values.length,
-                  (index) {
-                    bool selected = widget.values.values.toList()[index] ==
-                        widget.values[value];
-                    return SizeableListTile(
-                      title: widget.values.values.toList()[index],
-                      icon: selected ? Icon(Icons.check) : null,
-                      selected: selected,
-                      onTap: () => Navigator.pop(
-                        context,
-                        widget.values.keys.toList()[index],
-                      ),
-                    );
-                  },
-                ),
-              ),
-            );
-
-            if (result != null) {
-              widget.onValueChanged(result);
-              if (widget.cooldown != null) {
-                setState(() => coolingDown = true);
-                Future.delayed(Duration(milliseconds: widget.cooldown),
-                    () => setState(() => coolingDown = false));
-              }
-            }
-          },
+    return SizeableListTile(
+      title: widget.title,
+      subtitle: Text(
+        widget.values[value],
+        style: TextStyle(
+          color: Theme.of(context).textTheme.headline6.color.withAlpha(160),
         ),
       ),
+      selectedColor: widget.selectedColor,
+      icon: widget.icon,
+      onTap: () async {
+        var result = await showModalBottomSheet(
+          context: context,
+          isScrollControlled: false,
+          builder: (context) => ListView(
+            shrinkWrap: true,
+            physics: ClampingScrollPhysics(),
+            children: List.generate(
+              widget.values.length,
+              (index) {
+                bool selected = widget.values.values.toList()[index] ==
+                    widget.values[value];
+                return SizeableListTile(
+                  title: widget.values.values.toList()[index],
+                  icon: selected ? Icon(Icons.check) : null,
+                  selected: selected,
+                  onTap: () => Navigator.pop(
+                    context,
+                    widget.values.keys.toList()[index],
+                  ),
+                );
+              },
+            ),
+          ),
+        );
+
+        if (result != null) widget.onValueChanged(result);
+      },
     );
   }
 }
