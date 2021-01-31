07c41e415e3ff183b6922240f96b7dafc9cb38b8
SahilSonar <sss.sonar2003@gmail.com>
2021-01-31 16:07:17 +0530

    fries: Add navbar configs
    Signed-off-by: SahilSonar <sss.sonar2003@gmail.com>
    Change-Id: Ib32b39a19fb3e2572174945ae2ead801a7358d5c

--- lib/locales/locale_strings.g.dart
+++ lib/locales/locale_strings.g.dart
@@ -92,6 +92,12 @@ class _$SystemLocaleStrings {
   final String gesturesDoubleTapSleepLockscreenDesc = "system.gestures.double_tap_sleep_lockscreen.desc".tr();
   final String gesturesPulseOnNewTracksTitle = "system.gestures.pulse_on_new_tracks.title".tr();
   final String gesturesPulseOnNewTracksDesc = "system.gestures.pulse_on_new_tracks.desc".tr();
+  final String navigationTitle = "system.navigation.title".tr();
+  final String navigationSysuiNavBarHintTitle = "system.navigation.sysui_nav_bar_hint.title".tr();
+  final String navigationSysuiNavBarHintDesc = "system.navigation.sysui_nav_bar_hint.desc".tr();
+  final String navigationNavigationHandleWidthTitle = "system.navigation.navigation_handle_width.title".tr();
+  final String navigationSysuiNavBarInverseTitle = "system.navigation.sysui_nav_bar_inverse.title".tr();
+  final String navigationSysuiNavBarInverseDesc = "system.navigation.sysui_nav_bar_inverse.desc".tr();
   final String networkTitle = "system.network.title".tr();
   final String networkTetheringAllowVpnUpstreamsTitle = "system.network.tethering_allow_vpn_upstreams.title".tr();
   final String networkTetheringAllowVpnUpstreamsDesc = "system.network.tethering_allow_vpn_upstreams.desc".tr();
