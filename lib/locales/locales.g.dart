a11dfbf7c4c8cb17681a77c9c5fe718e3b10b279
Kshitij Gupta <kshitijgm@gmail.com>
2021-05-30 19:45:34 +0530

    fries: Generate strings, reformat, upgrade deps
    Change-Id: I9ad1e727bbd7f9adf2582263fe76ff0828033800

--- lib/locales/locales.g.dart
+++ lib/locales/locales.g.dart
@@ -5,11 +5,13 @@ class Locales {
 
   static List<Locale> get supported => [
         Locale("en", "US"),
+        Locale("es", "ES"),
         Locale("pt", "BR"),
       ];
 
   static Map<String, Map<String, String>> get data => {
         _$LocaleEnUS().locale: _$LocaleEnUS().data,
+        _$LocaleEsES().locale: _$LocaleEsES().data,
         _$LocalePtBR().locale: _$LocalePtBR().data,
       };
 }
@@ -25,46 +27,6 @@ class _$LocaleEnUS extends _$LocaleBase {
 
   @override
   Map<String, String> get data => {
-        "audiofx.title": "AudioFX",
-        "audiofx.status.on": "On",
-        "audiofx.status.off": "Off",
-        "audiofx.audio_preset.title": "Audio preset",
-        "audiofx.headset_profile.title": "Headset profile",
-        "audiofx.headset_profile.default": "Default mode",
-        "lockscreen.title": "Lockscreen",
-        "lockscreen.clocks.title": "Clocks",
-        "lockscreen.clocks.lock_screen_clock.title": "Lock Screen Clock",
-        "lockscreen.clocks.lock_screen_clock.v_default": "Default",
-        "lockscreen.clocks.lock_screen_clock.v_bubble": "Bubble",
-        "lockscreen.clocks.lock_screen_clock.v_analog": "Analog",
-        "lockscreen.clocks.lock_screen_clock.v_type": "Type",
-        "lockscreen.clocks.lock_screen_clock.v_bold": "Bold",
-        "lockscreen.clocks.lock_screen_clock.v_sammy": "Sammy",
-        "lockscreen.clocks.lock_screen_clock.v_sammybold": "Sammy bold",
-        "lockscreen.clocks.lock_screen_clock.v_sfuny": "SFUNY",
-        "lockscreen.albumart.title": "Album art",
-        "lockscreen.albumart.lockscreen_media_metadata.title":
-            "Lockscreen media art",
-        "lockscreen.albumart.lockscreen_media_metadata.desc":
-            "Show album art on lockscreen",
-        "lockscreen.albumart.lockscreen_media_blur.title":
-            "Lockscreen blur level",
-        "lockscreen.albumart.lockscreen_media_blur.desc":
-            "Configure blur intensity of lockscreen media art",
-        "lockscreen.pocket.title": "Pocket mode",
-        "lockscreen.pocket.pocket_judge.title": "Pocket detection",
-        "lockscreen.pocket.pocket_judge.desc":
-            "Block screen and button inputs when device is in pocket",
-        "lockscreen.tuner.title": "Lockscreen shortcuts",
-        "lockscreen.tuner.systemui_tuner_lockscreen.title": "Lockscreen tuner",
-        "lockscreen.tuner.systemui_tuner_lockscreen.desc":
-            "Manage lockscreen shortcuts",
-        "preferences.colorpicker.light_error": "Color is too light",
-        "preferences.colorpicker.dark_error": "Color is too dark",
-        "preferences.colorpicker.light_warning":
-            "Color is too light, it may be normalized!",
-        "preferences.colorpicker.dark_warning":
-            "Color is too dark, it may be normalized!",
         "qs.title": "Quick Settings",
         "qs.header.no_notifications": "No notifications",
         "qs.tweaks.title": "Quick settings tweaks",
@@ -233,6 +195,283 @@ class _$LocaleEnUS extends _$LocaleBase {
         "themes.themes.sysui_colors_active.title": "Colored SystemUI",
         "themes.themes.sysui_colors_active.desc":
             "Wallpaper colored scrims and QS",
+        "audiofx.title": "AudioFX",
+        "audiofx.status.on": "On",
+        "audiofx.status.off": "Off",
+        "audiofx.audio_preset.title": "Audio preset",
+        "audiofx.headset_profile.title": "Headset profile",
+        "audiofx.headset_profile.default": "Default mode",
+        "lockscreen.title": "Lockscreen",
+        "lockscreen.clocks.title": "Clocks",
+        "lockscreen.clocks.lock_screen_clock.title": "Lock Screen Clock",
+        "lockscreen.clocks.lock_screen_clock.v_default": "Default",
+        "lockscreen.clocks.lock_screen_clock.v_bubble": "Bubble",
+        "lockscreen.clocks.lock_screen_clock.v_analog": "Analog",
+        "lockscreen.clocks.lock_screen_clock.v_type": "Type",
+        "lockscreen.clocks.lock_screen_clock.v_bold": "Bold",
+        "lockscreen.clocks.lock_screen_clock.v_sammy": "Sammy",
+        "lockscreen.clocks.lock_screen_clock.v_sammybold": "Sammy bold",
+        "lockscreen.clocks.lock_screen_clock.v_sfuny": "SFUNY",
+        "lockscreen.albumart.title": "Album art",
+        "lockscreen.albumart.lockscreen_media_metadata.title":
+            "Lockscreen media art",
+        "lockscreen.albumart.lockscreen_media_metadata.desc":
+            "Show album art on lockscreen",
+        "lockscreen.albumart.lockscreen_media_blur.title":
+            "Lockscreen blur level",
+        "lockscreen.albumart.lockscreen_media_blur.desc":
+            "Configure blur intensity of lockscreen media art",
+        "lockscreen.pocket.title": "Pocket mode",
+        "lockscreen.pocket.pocket_judge.title": "Pocket detection",
+        "lockscreen.pocket.pocket_judge.desc":
+            "Block screen and button inputs when device is in pocket",
+        "lockscreen.tuner.title": "Lockscreen shortcuts",
+        "lockscreen.tuner.systemui_tuner_lockscreen.title": "Lockscreen tuner",
+        "lockscreen.tuner.systemui_tuner_lockscreen.desc":
+            "Manage lockscreen shortcuts",
+        "preferences.colorpicker.light_error": "Color is too light",
+        "preferences.colorpicker.dark_error": "Color is too dark",
+        "preferences.colorpicker.light_warning":
+            "Color is too light, it may be normalized!",
+        "preferences.colorpicker.dark_warning":
+            "Color is too dark, it may be normalized!",
+      };
+}
+
+class _$LocaleEsES extends _$LocaleBase {
+  @override
+  String get locale => "es-ES";
+
+  @override
+  Map<String, String> get data => {
+        "qs.title": "Ajustes rápidos",
+        "qs.header.no_notifications": "Sin notificaciones",
+        "qs.tweaks.title": "Configuración de ajustes rápidos",
+        "qs.tweaks.qs_show_auto_brightness.title": "Brillo automático en QS",
+        "qs.tweaks.qs_show_auto_brightness.desc":
+            "Mostrar icono de brillo automático en el panel de QS",
+        "qs.tweaks.qs_show_brightness_slider.title":
+            "Control deslizante del brillo",
+        "qs.tweaks.qs_show_brightness_slider.desc":
+            "Mostrar un control deslizante de brillo en el panel de configuración rápida",
+        "qs.tweaks.qs_show_brightness_slider.v_0": "Nunca mostrar",
+        "qs.tweaks.qs_show_brightness_slider.v_1":
+            "Mostrar mientras está expandido",
+        "qs.tweaks.qs_show_brightness_slider.v_2": "Mostrar siempre",
+        "qs.tweaks.qs_tile_title_visibility.title":
+            "Mostrar título de mosaicos en QS",
+        "qs.tweaks.qs_tile_title_visibility.desc":
+            "Ocultar o mostrar el título de los mosaicos en QS",
+        "qs.tweaks.qs_rows_portrait.title": "Filas de iconos en modo vertical",
+        "qs.tweaks.qs_columns_portrait.title":
+            "Columnas de iconos en modo vertical",
+        "qs.tweaks.qs_rows_landscape.title":
+            "Hileras de iconos en modo paisaje",
+        "qs.tweaks.qs_columns_landscape.title": "Tiles columns on landscape",
+        "qs.colors.title": "Colores de ajustes rápidos",
+        "qs.colors.qs_panel_bg_use_fw.title":
+            "Valores predeterminados del marco",
+        "qs.colors.qs_panel_bg_use_fw.desc":
+            "Deshabilite las personalizaciones de color QS y de malla",
+        "qs.colors.qs_panel_bg_color.title": "Color de fondo",
+        "qs.colors.qs_panel_bg_color.desc": "Cambia el color de tu panel de QS",
+        "qs.colors.qs_panel_bg_alpha.title": "Background alpha",
+        "qs.colors.qs_panel_bg_disco.title": "Disco de iconos en QS",
+        "qs.colors.qs_panel_bg_disco.desc":
+            "¡Haz que tus columnas de QS sean coloridos!",
+        "qs.colors.qs_tile_tint.title": "Tinte para columnas en  QS",
+        "qs.colors.qs_tile_tint.desc":
+            "Tinte de columnas de QS con color de acento",
+        "statusbar.title": "Barra de estado",
+        "statusbar.cutouts.title": "Recortes en pantalla",
+        "statusbar.cutouts.display_cutout_mode.title": "Modo de recorte",
+        "statusbar.cutouts.display_cutout_mode.v_0": "Normal",
+        "statusbar.cutouts.display_cutout_mode.v_1": "Sumergido",
+        "statusbar.cutouts.display_cutout_mode.v_2": "Esconder",
+        "statusbar.cutouts.stock_statusbar_in_hide.title":
+            "Barra de estado por defecto en Ocultar",
+        "statusbar.cutouts.stock_statusbar_in_hide.desc":
+            "Use la altura de la barra de estado predeterminada (generalmente más pequeña) en el modo ocultar",
+        "statusbar.cutouts.sysui_rounded_size.title":
+            "Radio de esquina redonda",
+        "statusbar.clock.title": "Reloj",
+        "statusbar.clock.smartclock.title": "Reloj inteligente",
+        "statusbar.clock.smartclock.desc":
+            "Muestre el reloj periódicamente durante un breve período",
+        "statusbar.icons.title": "Iconos de la barra de estado",
+        "statusbar.icons.show_fourg_icon.title": "Icono 4G",
+        "statusbar.icons.show_fourg_icon.desc":
+            "Muestra el icono 4G en la barra de estado en lugar de LTE",
+        "statusbar.icons.show_volte_icon.title": "Icono de VoLTE",
+        "statusbar.icons.show_volte_icon.desc":
+            "Mostrar icono HD en la barra de estado para VoLTE",
+        "statusbar.icons.roaming_indicator_icon.title": "Icono de Itinerancia",
+        "statusbar.icons.roaming_indicator_icon.desc":
+            "Mostrar el icono de roaming en la barra de estado",
+        "statusbar.icons.systemui_tuner_statusbar.title":
+            "Iconos de la barra de estado del sistema",
+        "statusbar.icons.systemui_tuner_statusbar.desc":
+            "Deshabilitar / habilitar los iconos del sistema desde la barra de estado",
+        "statusbar.nettraffic.title": "Monitor de tráfico de red",
+        "statusbar.nettraffic.network_traffic_location.title":
+            "Modo de visualización",
+        "statusbar.nettraffic.network_traffic_location.v_0": "Desactivado",
+        "statusbar.nettraffic.network_traffic_location.v_1": "Barra de estado",
+        "statusbar.nettraffic.network_traffic_location.v_2": "Ajustes rápidos",
+        "statusbar.nettraffic.network_traffic_autohide.title":
+            "Esconderlo automáticamente",
+        "statusbar.nettraffic.network_traffic_autohide.desc":
+            "Ocultar el monitor de tráfico cuando no hay actividad",
+        "statusbar.nettraffic.network_traffic_unit_type.title":
+            "Unidades de tráfico",
+        "statusbar.nettraffic.network_traffic_unit_type.v_0": "Bytes",
+        "statusbar.nettraffic.network_traffic_unit_type.v_1": "Bits",
+        "statusbar.battery.title": "Bateria",
+        "statusbar.battery.status_bar_battery_style.title":
+            "Estilo de la Bateria",
+        "statusbar.battery.status_bar_battery_style.v_0": "Portrato",
+        "statusbar.battery.status_bar_battery_style.v_1": "Círculo",
+        "statusbar.battery.status_bar_battery_style.v_2": "Círculo punteado",
+        "statusbar.battery.status_bar_battery_style.v_3": "Círculo sólido",
+        "statusbar.battery.status_bar_battery_style.v_4": "Texto",
+        "statusbar.battery.status_bar_battery_style.v_5": "Oculto",
+        "statusbar.battery.status_bar_show_battery_percent.title":
+            "Porcentaje de la batería",
+        "statusbar.battery.status_bar_show_battery_percent.v_0": "Oculto",
+        "statusbar.battery.status_bar_show_battery_percent.v_1":
+            "Dentro del ícono",
+        "statusbar.battery.status_bar_show_battery_percent.v_2":
+            "Al lado del ícono",
+        "statusbar.battery.qs_header_show_battery_percent.title":
+            "Porcentaje de batería en QS",
+        "statusbar.battery.qs_header_show_battery_percent.desc":
+            "Muestra el porcentaje de batería en el encabezado de QS en lugar de la estimación",
+        "system.title": "Sistema & Gestos",
+        "system.buttons.title": "Botones",
+        "system.buttons.volume_button_music_control.title":
+            "Botones de volumen para saltar pistas",
+        "system.buttons.volume_button_music_control.desc":
+            "Mantenga presionados los botones de volumen para avanzar / retroceder pista",
+        "system.buttons.advanced_reboot.title": "Reinicio avanzado",
+        "system.buttons.advanced_reboot.desc":
+            "Mostrar opciones de reinicio adicionales",
+        "system.gestures.title": "Gestos",
+        "system.gestures.double_tap_sleep_lockscreen.title":
+            "Doble toque para dormir en la pantalla de bloqueo",
+        "system.gestures.double_tap_sleep_lockscreen.desc":
+            "Apague la pantalla tocando dos veces el espacio vacío en la pantalla de bloqueo",
+        "system.gestures.three_finger_gesture.title":
+            "Captura de pantalla de tres dedos",
+        "system.gestures.three_finger_gesture.desc":
+            "Desliza tres dedos hacia abajo para hacer una captura de pantalla",
+        "system.gestures.pulse_on_new_tracks.title":
+            "Pistas de música ambiental",
+        "system.gestures.pulse_on_new_tracks.desc":
+            "Mostrar pantalla ambiental cuando se reproduce una nueva pista de música",
+        "system.navigation.title": "Navegación",
+        "system.navigation.sysui_nav_bar_hint.title": "Mostrar barra de gestos",
+        "system.navigation.sysui_nav_bar_hint.desc":
+            "Alternar navegación/barra",
+        "system.navigation.navigation_handle_width.title":
+            "Longitud de la manija de navegación",
+        "system.navigation.sysui_nav_bar_inverse.title":
+            "Cambiar los botones de navegación",
+        "system.navigation.sysui_nav_bar_inverse.desc":
+            "Cambie los botones de navegación cuando use el diseño de 3 botones",
+        "system.navigation.systemui_tuner_navbar.title":
+            "Personalización de la barra de navegación",
+        "system.navigation.systemui_tuner_navbar.desc":
+            "Cambiar el diseño de la barra de navegación",
+        "system.network.title": "Internet",
+        "system.network.tethering_allow_vpn_upstreams.title":
+            "Permitir a los clientes usar VPN",
+        "system.network.tethering_allow_vpn_upstreams.desc":
+            "Permitir que los clientes de hotspot / tethering utilicen la conexión VPN de este dispositivo",
+        "system.packagemanager.title": "Manejador de paquetes",
+        "system.packagemanager.allow_signature_fake.title":
+            "Suplantación de la firma del paquete",
+        "system.packagemanager.allow_signature_fake.desc":
+            "Permitir firmas falsas (útil para microG)",
+        "system.notifications.title": "Notificaciones",
+        "system.notifications.less_boring_heads_up.title":
+            "Cabeceras menos aburridas",
+        "system.notifications.less_boring_heads_up.desc":
+            "Reciba solo avisos de las aplicaciones de llamadas y mensajería",
+        "themes.title": "Temas",
+        "themes.header.search_settings": "Buscar ajustes",
+        "themes.themes.title": "Temas",
+        "themes.themes.system_accent.title": "Color de acento",
+        "themes.themes.system_accent.desc": "Toma tu color favorito!",
+        "themes.themes.system_accent.cancel": "Cancelar",
+        "themes.themes.system_accent.confirm": "Confirmar",
+        "themes.themes.system_accent.light_color.title": "Claro",
+        "themes.themes.system_accent.light_color.light_warning":
+            "El color claro es demasiado claro",
+        "themes.themes.system_accent.light_color.dark_warning":
+            "El color de la luz es demasiado oscuro",
+        "themes.themes.system_accent.dark_color.title": "Oscuro",
+        "themes.themes.system_accent.dark_color.light_warning":
+            "El color oscuro es demasiado claro",
+        "themes.themes.system_accent.dark_color.dark_warning":
+            "El color oscuro es demasiado oscuro",
+        "themes.themes.system_accent.calculate_shades_label":
+            "Calcula automáticamente los tonos alternativos",
+        "themes.themes.system_icon_shape.title": "Forma de icono",
+        "themes.themes.system_icon_pack.title": "Paquete de iconos del sistema",
+        "themes.themes.systemui_plugin_volume.title": "Panel del volumen",
+        "themes.themes.systemui_plugin_volume.v_aosp": "AOSP",
+        "themes.themes.systemui_plugin_volume.v_compact": "Compacto",
+        "themes.themes.systemui_plugin_volume.v_oreo": "Oreo",
+        "themes.themes.systemui_plugin_volume.v_tiled": "Tiled",
+        "themes.themes.volume_panel_on_left.title":
+            "Dirección del panel de volumen",
+        "themes.themes.volume_panel_on_left.v_0": "Derecho",
+        "themes.themes.volume_panel_on_left.v_1": "Izquierdo",
+        "themes.themes.sysui_colors_active.title": "SystemUI colorido",
+        "themes.themes.sysui_colors_active.desc":
+            "Fonto de pantalla colorido y QS",
+        "audiofx.title": "AudioFX",
+        "audiofx.status.on": "Activado",
+        "audiofx.status.off": "Desactivado",
+        "audiofx.audio_preset.title": "Preajuste de audio",
+        "audiofx.headset_profile.title": "Perfil de auriculares",
+        "audiofx.headset_profile.default": "Modo por defecto",
+        "lockscreen.title": "Pantalla de Bloqueo",
+        "lockscreen.clocks.title": "Relojes",
+        "lockscreen.clocks.lock_screen_clock.title":
+            "Reloj de pantalla de bloqueo",
+        "lockscreen.clocks.lock_screen_clock.v_default": "Por defecto",
+        "lockscreen.clocks.lock_screen_clock.v_bubble": "Burbuja",
+        "lockscreen.clocks.lock_screen_clock.v_analog": "Analógico",
+        "lockscreen.clocks.lock_screen_clock.v_type": "Tipo",
+        "lockscreen.clocks.lock_screen_clock.v_bold": "Negrita",
+        "lockscreen.clocks.lock_screen_clock.v_sammy": "Sammy",
+        "lockscreen.clocks.lock_screen_clock.v_sammybold": "Sammy negrita",
+        "lockscreen.clocks.lock_screen_clock.v_sfuny": "SFUNY",
+        "lockscreen.albumart.title": "Arte del albúm",
+        "lockscreen.albumart.lockscreen_media_metadata.title":
+            "Arte multimedia de pantalla de bloqueo",
+        "lockscreen.albumart.lockscreen_media_metadata.desc":
+            "Mostrar la carátula del álbum en la pantalla de bloqueo",
+        "lockscreen.albumart.lockscreen_media_blur.title":
+            "Nivel de desenfoque de la pantalla de bloqueo",
+        "lockscreen.albumart.lockscreen_media_blur.desc":
+            "Configurar la intensidad de desenfoque del arte multimedia en la pantalla de bloqueo",
+        "lockscreen.pocket.title": "Modo bolsillo",
+        "lockscreen.pocket.pocket_judge.title": "Detección en bolsillo",
+        "lockscreen.pocket.pocket_judge.desc":
+            "Bloquear las entradas de la pantalla y los botones cuando el dispositivo está en el bolsillo",
+        "lockscreen.tuner.title": "Atajos de pantalla de bloqueo",
+        "lockscreen.tuner.systemui_tuner_lockscreen.title":
+            "Sintonizador de pantalla de bloqueo",
+        "lockscreen.tuner.systemui_tuner_lockscreen.desc":
+            "Administrar atajos de pantalla de bloqueo",
+        "preferences.colorpicker.light_error": "El color es demasiado claro",
+        "preferences.colorpicker.dark_error": "El color es demasiado oscuro",
+        "preferences.colorpicker.light_warning":
+            "El color es demasiado claro, ¡puede estar normalizado!",
+        "preferences.colorpicker.dark_warning":
+            "El color es demasiado oscuro, ¡puede estar normalizado!",
       };
 }
 
