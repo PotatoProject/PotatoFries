36fdb767d0271de116bac5c823db03747055e7a3
SahilSonar <sss.sonar2003@gmail.com>
2021-11-19 16:14:40 +0530

    fries: Initial clean-up for frico-release
    Change-Id: I13066366885fb6b0c92a0986a088cdcf5cf2ce84

--- lib/data/lock_screen.dart
+++ lib/data/lock_screen.dart
@@ -7,67 +7,10 @@ import 'package:potato_fries/ui/smart_icon.dart';
 final PageData lockScreen = PageData(
   key: 'lock_screen',
   categories: [
-    PageCategoryData(LocaleStrings.lockscreen.clocksTitle, clocks),
-    PageCategoryData(LocaleStrings.lockscreen.albumartTitle, albumArt),
-    PageCategoryData(LocaleStrings.lockscreen.pocketTitle, pocketMode),
-    PageCategoryData(LocaleStrings.lockscreen.tunerTitle, tunerTitle),
+    // PageCategoryData(LocaleStrings.lockscreen.clocksTitle, clocks),
   ],
 );
 
 final List<Preference> clocks = [
-  CustomPreference(id: 'LockScreenClockPicker'),
-];
-
-final List<Preference> albumArt = [
-  SettingPreference.withSwitch(
-    title: LocaleStrings.lockscreen.albumartLockscreenMediaMetadataTitle,
-    description: LocaleStrings.lockscreen.albumartLockscreenMediaMetadataDesc,
-    icon: SmartIconData.iconData(Icons.image),
-    setting: 'lockscreen_media_metadata',
-    type: SettingType.SECURE,
-    options: SwitchOptions(
-      defaultValue: true,
-    ),
-    minVersion: '4.0.0',
-  ),
-  SettingPreference.withSlider(
-    title: LocaleStrings.lockscreen.albumartLockscreenMediaBlurTitle,
-    setting: 'lockscreen_media_blur',
-    type: SettingType.SYSTEM,
-    options: SliderOptions(
-      max: 25,
-      defaultValue: 25,
-    ),
-    dependencies: [
-      SettingDependency.boolean(
-        name: 'lockscreen_media_metadata',
-        type: SettingType.SECURE,
-        value: true,
-      ),
-    ],
-    minVersion: '4.0.0',
-  ),
-];
-
-final List<Preference> pocketMode = [
-  SettingPreference.withSwitch(
-    title: LocaleStrings.lockscreen.pocketPocketJudgeTitle,
-    description: LocaleStrings.lockscreen.pocketPocketJudgeDesc,
-    setting: 'pocket_judge',
-    type: SettingType.SYSTEM,
-    options: SwitchOptions(
-      defaultValue: false,
-    ),
-    minVersion: '4.1.2',
-  ),
-];
-
-final List<Preference> tunerTitle = [
-  ActivityPreference(
-    title: LocaleStrings.lockscreen.tunerSystemuiTunerLockscreenTitle,
-    description: LocaleStrings.lockscreen.tunerSystemuiTunerLockscreenDesc,
-    cls: "com.android.systemui.tuner.LockscreenActivity",
-    pkg: "com.android.systemui",
-    minVersion: '4.0.2',
-  ),
+  // CustomPreference(id: 'LockScreenClockPicker'),
 ];
--- lib/data/themes.dart
+++ lib/data/themes.dart
@@ -8,64 +8,23 @@ import 'package:android_flutter_settings/android_flutter_settings.dart';
 final PageData themes = PageData(
   key: 'themes',
   categories: [
-    PageCategoryData(LocaleStrings.themes.themesTitle, theme),
+    // PageCategoryData(LocaleStrings.themes.themesTitle, theme),
   ],
 );
 
 final List<Preference> theme = [
-  CustomPreference(id: 'AccentPicker'),
-  SettingPreference.withSwitch(
-    setting: 'sysui_colors_active',
-    title: LocaleStrings.themes.themesSysuiColorsActiveTitle,
-    description: LocaleStrings.themes.themesSysuiColorsActiveDesc,
-    icon: SmartIconData.iconData(MdiIcons.selectColor),
-    type: SettingType.SYSTEM,
-    options: SwitchOptions(
-      defaultValue: true,
-    ),
-    minVersion: '4.1.2',
-    dependencies: [
-      SettingDependency.boolean(
-        name: 'qs_tiles_bg_disco',
-        type: SettingType.SYSTEM,
-        value: false,
-      ),
-    ],
-  ),
-  CustomPreference(id: 'IconShapePicker'),
-  CustomPreference(id: 'IconPackPicker'),
-  SettingPreference.withDropdown(
-    setting: 'systemui_plugin_volume',
-    title: LocaleStrings.themes.themesSystemuiPluginVolumeTitle,
-    icon: SmartIconData.iconData(Icons.music_note),
-    type: SettingType.SYSTEM,
-    options: DropdownOptions(
-      values: {
-        'co.potatoproject.plugin.volume.aosp':
-            LocaleStrings.themes.themesSystemuiPluginVolumeVAosp,
-        'co.potatoproject.plugin.volume.compact':
-            LocaleStrings.themes.themesSystemuiPluginVolumeVCompact,
-        'co.potatoproject.plugin.volume.oreo':
-            LocaleStrings.themes.themesSystemuiPluginVolumeVOreo,
-        'co.potatoproject.plugin.volume.tiled':
-            LocaleStrings.themes.themesSystemuiPluginVolumeVTiled,
-      },
-      defaultValue: 'co.potatoproject.plugin.volume.aosp',
-    ),
-    minVersion: '4.0.0',
-  ),
-  SettingPreference.withDropdown(
-    setting: 'volume_panel_on_left',
-    title: LocaleStrings.themes.themesVolumePanelOnLeftTitle,
-    icon: SmartIconData.iconData(MdiIcons.arrowLeftRight),
-    type: SettingType.SYSTEM,
-    options: DropdownOptions(
-      values: {
-        '0': LocaleStrings.themes.themesVolumePanelOnLeftV0,
-        '1': LocaleStrings.themes.themesVolumePanelOnLeftV1,
-      },
-      defaultValue: '0',
-    ),
-    minVersion: '4.0.0',
-  ),
+  // CustomPreference(id: 'AccentPicker'),
+  // SettingPreference.withSwitch(
+  //   setting: 'sysui_colors_active',
+  //   title: LocaleStrings.themes.themesSysuiColorsActiveTitle,
+  //   description: LocaleStrings.themes.themesSysuiColorsActiveDesc,
+  //   icon: SmartIconData.iconData(MdiIcons.selectColor),
+  //   type: SettingType.SYSTEM,
+  //   options: SwitchOptions(
+  //     defaultValue: true,
+  //   ),
+  //   minVersion: '4.1.2',
+  // ),
+  // CustomPreference(id: 'IconShapePicker'),
+  // CustomPreference(id: 'IconPackPicker'),
 ];
